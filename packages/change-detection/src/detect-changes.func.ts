74a44148a402444095420ce510945539f0a89c19
Diabeu <scibor.rudnicki@ster-project.pl>
2017-12-04 19:18:52 +0100

    feat(DetectChanges): add prototyped ngAfterViewInit to detach component.

--- packages/change-detection/src/detect-changes.func.ts
+++ packages/change-detection/src/detect-changes.func.ts
@@ -14,21 +14,31 @@ import { PropertiesInterface } from './interface';
 export function DetectChanges(properties: PropertiesInterface): Function {
   return function (target: any) {
     let dcClass = new DetectChangesClass({ getter: {}, setter: {} }, target, properties);
-
-    // store ngOnInit
+    // store ngOnInit.
     const ngOnInit = target.prototype.ngOnInit;
+    // store ngAfterViewInit
+    const ngAfterViewInit = target.prototype.ngAfterViewInit;
     // store old getters and setters.
     dcClass.lookupProperties(properties);
-
-    // ngOnInit
+    // ngOnInit.
     target.prototype.ngOnInit = function (): void {
       // set new setters and getters specified in `properties` object.
       dcClass.setterDetectChanges(this);
-
       // Apply original `ngOnInit()` method.
       if (ngOnInit !== undefined) {
         ngOnInit.apply(this, arguments);
       }
     };
+    // ngAfterViewInit.
+    target.prototype.ngAfterViewInit = function (): void {
+      // Detach component from change detector after view init.
+      if (this.cd) {
+        dcClass.detach(this, this.cd);
+      }
+      // Apply original `ngAfterViewInit()` method.
+      if (ngAfterViewInit !== undefined) {
+        ngAfterViewInit.apply(this, arguments);
+      }
+    }
   };
 }
