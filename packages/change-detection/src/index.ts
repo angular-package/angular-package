6f196e56c591e4551ade0f791a3a9e8d7e7ce438
angularpackage <angular-package@wwwdev.io>
2017-12-11 18:27:34 +0100

    feat(ChangeDetectorAbstractClass): add abstract class to help integrate ChangeDetection

--- packages/change-detection/src/index.ts
+++ packages/change-detection/src/index.ts
@@ -1,1 +1,3 @@
-export { DetectChanges } from './detect-changes.func';
+export { ChangeDetection } from './change-detection.decorator';
+export { DetectChanges } from './detect-changes.decorator';
+export { ChangeDetectorAbstractClass } from './change-detector.abstract';
