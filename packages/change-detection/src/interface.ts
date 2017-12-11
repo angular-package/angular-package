ce0d3b5768a7bc574aa1c8c136747abbd1a7c0a7
angularpackage <angular-package@wwwdev.io>
2017-12-11 18:27:53 +0100

    chore: update interface

--- packages/change-detection/src/interface.ts
+++ packages/change-detection/src/interface.ts
@@ -1,10 +1,26 @@
-export interface PropertySettingsInterface {
-  changeDetection: boolean;
+import { ChangeDetectorClass } from './change-detector.class';
+
+export interface ChangeDetectionInterface {
+
+  /**
+   * Status component detection. When true `Detached`, false means `CheckOnce`.
+   * @type {boolean}
+   * @memberof ChangeDetectionInterface
+   */
+  detection: boolean;
+  properties: PropertiesInterface;
+  ready:boolean;
 }
+
+export interface LookupItemInterface {
+  [key: string]: Function;
+}
+
+export interface LookupInterface {
+  getter: LookupItemInterface;
+  setter: LookupItemInterface;
+}
+
 export interface PropertiesInterface {
-  [index: string]: PropertySettingsInterface;
-}
-export interface LookUpInterface {
-  getter: Object;
-  setter: Object;
+  [index: string]: boolean;
 }
