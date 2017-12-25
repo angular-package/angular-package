8bdfb12494d9d5a8b17f01f242f3101cc422515d
angularpackage <angular-package@wwwdev.io>
2017-12-25 16:47:27 +0100

    chore(rollup.config): add globals and external

--- packages/change-detection/rollup.config.js
+++ packages/change-detection/rollup.config.js
@@ -1,5 +1,14 @@
 import rollupDefaultConfig from './../../rollup.config.js';
 
-const config = rollupDefaultConfig('change-detection');
+const globals = {
+  '@angular/core': 'ng.core',
+  'lodash-es': '_'
+};
+const external = Object.keys(globals);
+
+const config = rollupDefaultConfig('changeDetection', {
+  external,
+  globals
+});
 
 export default config;
