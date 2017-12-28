9804ad2f0e5270a6ee743301f44e9aaf55b7eb48
angularpackage <angular-package@wwwdev.io>
2017-12-28 17:48:47 +0100

    chore(rollup.config): usage of new rollupDefaultConfig

--- packages/change-detection/rollup.config.js
+++ packages/change-detection/rollup.config.js
@@ -6,9 +6,6 @@ const globals = {
 };
 const external = Object.keys(globals);
 
-const config = rollupDefaultConfig('changeDetection', {
-  external,
-  globals
-});
+const config = rollupDefaultConfig('changeDetection', { external }, { globals });
 
 export default config;
