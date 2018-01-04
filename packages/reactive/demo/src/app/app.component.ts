b9b3176bce4a7cf734b30f1357c50c88b3337d1b
angularpackage <angular-package@wwwdev.io>
2018-01-04 16:16:43 +0100

    refactor(AppComponent): add example to app.component.ts

--- packages/reactive/demo/src/app/app.component.ts
+++ packages/reactive/demo/src/app/app.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { Subscribe } from '@angular-package/reactive/decorator/subscribe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+@Subscribe<string>(['name'])
+@Subscribe<number>(['age'])
+export class AppComponent implements OnInit {
+
+  public nameSubscription: Subscription;
+  public ageSubscription: Subscription;
+
+  public name: string;
+  public name$: Observable<string>;
+
+  public age$: Observable<number>;
+  public age_: number;
+  set age(value: number) {
+    this.age_ = value;
+  }
+  get age(): number {
+    return this.age_;
+  }
+
+  ngOnInit() {
+    this.nameSubscription = this.name$.subscribe({
+      next: (value: string) => {
+        console.log(value);
+      }
+    });
+
+    this.ageSubscription = this.age$.subscribe({
+      next: (value: number) => {
+        console.log(value);
+      }
+    });
+
+    this.age = 27;
+    this.name = `Brayan`;
+  }
 }
