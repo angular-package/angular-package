330566c25cf156149844512216959bc0d20565a7
angularpackage <angular-package@wwwdev.io>
2018-01-12 13:52:13 +0100

    refactor(change-detection): move to folder src

--- packages/change-detection/packages/decorator/index.ts
+++ packages/change-detection/packages/decorator/index.ts
@@ -1,3 +1,1 @@
-export { ChangeDetection } from './change-detection.decorator';
-// export { DetectChanges } from './detect-changes.decorator';
-// export { ChangeDetectorAbstractClass } from './change-detector.abstract';
+export * from './src';
