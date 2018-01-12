d6bbfed11e19b4b48bc59a503ea52e4f59aa5385
angularpackage <angular-package@wwwdev.io>
2018-01-12 13:19:19 +0100

    refactor(change-detection): move all files from packages to subfolder decorator

---
+++ packages/change-detection/packages/decorator/index.ts
@@ -0,0 +1,3 @@
+export { ChangeDetection } from './change-detection.decorator';
+// export { DetectChanges } from './detect-changes.decorator';
+// export { ChangeDetectorAbstractClass } from './change-detector.abstract';
