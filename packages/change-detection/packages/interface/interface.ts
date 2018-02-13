5dbb13e9ab1b99699fb51958749c458f20e65bcd
angularpackage <angular-package@wwwdev.io>
2018-02-13 12:57:49 +0100

    refactor(ApPropertiesInterface): add interface
    Structure of `_properties` component variable.

---
+++ packages/change-detection/packages/interface/interface.ts
@@ -0,0 +1,9 @@
+/**
+ * Structure of `_properties` component variable.
+ * @example { signed: false, completed: true, z: true }
+ * @export
+ * @interface ApPropertiesInterface
+ */
+export interface ApPropertiesInterface {
+  [index: string]: boolean;
+}
