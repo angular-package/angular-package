1ea503909070f6e099b6c385a033ed8674445dcf
angularpackage <angular-package@wwwdev.io>
2018-02-28 14:06:57 +0100

    test: primary index

--- packages/core/test/index.ts
+++ packages/core/test/index.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import 'core-js/es6';
 import 'core-js/es7/reflect';
-
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy';
