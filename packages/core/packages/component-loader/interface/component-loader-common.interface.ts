323dcb647aa1d3908785ba31546dd2831060c4d4
angularpackage <angular-package@wwwdev.io>
2018-03-14 15:53:48 +0100

    refactor(ComponentLoaderCommonInterface): rebuild
    Add common properties for ComponentLoader like
    __component, __componentPropertyName, __create
    __destroy, __prefix, __suffix, __assign, __set, __get, __subscribe

--- packages/core/packages/component-loader/interface/component-loader-common.interface.ts
+++ packages/core/packages/component-loader/interface/component-loader-common.interface.ts
@@ -1,17 +1,23 @@
-import { ComponentFactoryResolver } from '@angular/core';
-import { ComponentType } from '../../type';
+import { ComponentFactoryResolver, ComponentRef, Type } from '@angular/core';
 
 /**
  * @export
  * @interface ComponentLoaderCommonInterface
+ * @template T
  */
-export interface ComponentLoaderCommonInterface {
-  __component: any;
-  __componentPropertyName: string;
-  __properties: string[];
-  componentFactoryResolver: ComponentFactoryResolver;
+export interface ComponentLoaderCommonInterface<T> {
 
-  __assign: (p: string | Array<string>, prefix: any, suffix: any, source: any) => void;
-  __get: <T>(property: string) => T | undefined;
-  __subscribe: (property: string, ...args: any[]) => void;
+  __component?: ComponentRef<T>;
+  __componentPropertyName?: string;
+  __create?: <D = T>(component: Type<D>) => this;
+  __destroy?: () => any;
+  __properties?: string[];
+  __prefix?: string;
+  __suffix?: string;
+
+  __assign?: <S>(p: string | string[], source: S) => void;
+  __set?: <PT>(property: string, value: PT) => void;
+  __get?: <PT>(property: string) => PT | undefined;
+  __subscribe?: (property: string, ...args: any[]) => void;
+
 }
