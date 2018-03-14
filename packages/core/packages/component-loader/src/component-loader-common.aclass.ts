2576df5ae728be8f0af46cb350254e84595ef80c
angularpackage <angular-package@wwwdev.io>
2018-03-14 17:27:35 +0100

    refactor(ComponentLoaderCommonAClass):  add properties, methods, update.
    Add:
    - Component Template <T>.
    - Property __prefix, __suffix, connectClass.
    - Add method __wrap to connect properties.
    
    Change:
    - __component value is ComponentRef<T> instead of any.
    - __assign() method got source and property types.
    - __get() and __set() methods property types names change.

--- packages/core/packages/component-loader/src/component-loader-common.aclass.ts
+++ packages/core/packages/component-loader/src/component-loader-common.aclass.ts
@@ -1,9 +1,11 @@
 // external
-import { ComponentFactoryResolver } from '@angular/core';
+import { ComponentFactoryResolver, ComponentRef, Type } from '@angular/core';
 
 // internal
-import { ComponentType } from '../../type';
 import { ComponentLoaderCommonInterface } from '../interface';
+import { ConnectClass } from '../../connect';
+import { CallbackGetterType } from '../../connect/type/callback-getter.type';
+import { CallbackSetterType } from '../../connect/type/callback-setter.type';
 
 /**
  * Some useful methods to handle dynamic component.
@@ -11,7 +13,14 @@ import { ComponentLoaderCommonInterface } from '../interface';
  * @abstract
  * @class ComponentLoaderCommonAClass
  */
-export abstract class ComponentLoaderCommonAClass implements ComponentLoaderCommonInterface {
+export
+  abstract class ComponentLoaderCommonAClass<T>
+  implements ComponentLoaderCommonInterface<T> {
+
+  public __prefix = '_';
+  public __suffix = '';
+
+  protected connectClass?: ConnectClass;
 
   /**
    * Property name where dynamic component will be placed.
@@ -23,7 +32,7 @@ export abstract class ComponentLoaderCommonAClass implements ComponentLoaderComm
    * Wrapper `set` for `__componentPropertyName`.
    * @memberof ComponentLoaderCommonAClass
    */
-  set __component(value: any) {
+  set __component(value: ComponentRef<T>) {
     if (this.__componentPropertyName) {
       this[this.__componentPropertyName] = value;
     }
@@ -34,11 +43,8 @@ export abstract class ComponentLoaderCommonAClass implements ComponentLoaderComm
    * @type {*}
    * @memberof ComponentLoaderCommonAClass
    */
-  get __component(): any {
-    if (this.__componentPropertyName) {
-      return this[this.__componentPropertyName];
-    }
-    return null;
+  get __component(): ComponentRef<T> {
+    return this[this.__componentPropertyName];
   }
 
   /**
@@ -48,61 +54,55 @@ export abstract class ComponentLoaderCommonAClass implements ComponentLoaderComm
   public __properties: string[] = [];
 
   /**
+   * Creates an instance of ComponentLoaderCommonAClass.
    * @param {ComponentFactoryResolver} componentFactoryResolver
    * @memberof ComponentLoaderCommonAClass
    */
-  constructor(public componentFactoryResolver: ComponentFactoryResolver) { }
+  constructor(public componentFactoryResolver: ComponentFactoryResolver) {}
 
   /**
-   * Assign property or list of properties from source object to targeted dynamic component instance.
-   * @public
-   * @param {(string | string[])} p Target property of dynamic component instance.
-   * @param {string} [prefix='_'] Source property prefix.
-   * @param {string} [suffix=''] Source property suffix.
-   * @param {*} [source=this]
+   * Assign values of property or list of properties from source component to dynamic component instance.
+   * @template PT Property type - not useful here.
+   * @template S Source component type.
+   * @param {(string | string[])} [p=this.__properties] Property that values will be set from source component to dynamic component.
+   * @param {S} source Component which properties values will be assigned to dynamic component.
    * @memberof ComponentLoaderCommonAClass
    */
-  public __assign(p: string | string[], prefix = '', suffix = '', source: any = this): void {
+  public __assign<PT, S>(p: string | string[] = this.__properties, source: S): void {
     if (this.__component) {
       if (p instanceof Array) {
         p.forEach((property: string): void => {
-          if (property) {
-            this.__set(property, source[`${prefix}${property}${suffix}`]);
-            // this.__component.instance[property] = source[`${prefix}${property}${suffix}`];
-          }
+          this.__set<PT>(property, source[property]);
         });
       } else if (p) {
-        this.__set(p, source[`${prefix}${p}${suffix}`]);
-        // this.__component.instance[p] = source[`${prefix}${p}${suffix}`];
+        this.__set<PT>(p, source[p]);
       }
     }
   }
 
   /**
-   * Set property value to dynamic component instance.
-   * @public
-   * @template T
-   * @param {string} key
-   * @param {T} value
+   * Set specified property value to dynamic component instance.
+   * @template PT Property type.
+   * @param {string} property Name of property that will be set to dynamic component instance.
+   * @param {PT} value Value of property that will be set to dynamic component instance.
    * @memberof ComponentLoaderCommonAClass
    */
-  public __set<T>(key: string, value: T): void {
+  public __set<PT>(property: string, value: PT): void {
     if (this.__component && this.__component.hasOwnProperty('instance')) {
       Object.assign(this.__component.instance, {
-        [key]: value
+        [property]: value
       });
     }
   }
 
   /**
    * Get specified property value from dynamic component instance.
-   * @public
-   * @template T Property type.
+   * @template PT Property type.
    * @param {string} property Name of property that will be get from instance.
-   * @returns {(T | undefined)} Return with specified type or undefined.
+   * @returns {(PT | undefined)} Return value with specified type or undefined.
    * @memberof ComponentLoaderCommonAClass
    */
-  public __get<T>(property: string): T | undefined {
+  public __get<PT>(property: string): PT | undefined {
     if (property.length > 0) {
       if (this.__component && this.__component.hasOwnProperty('instance')) {
         return this.__component.instance[property];
@@ -113,15 +113,14 @@ export abstract class ComponentLoaderCommonAClass implements ComponentLoaderComm
   }
 
   /**
-   * Subscribe to property of dynamic component instance.
-   * @public
-   * @param {string} property Property name to instance.
+   * Subscribe to specified property of dynamic component instance.
+   * @param {string} property Property name of dynamic component instance.
    * @param {...any[]} args Functions in order success, error, complete.
    * @memberof ComponentLoaderCommonAClass
    */
   public __subscribe(property: string, ...args: any[]): void {
     if (this.__component.instance.hasOwnProperty(property)) {
-      this.__component.instance[property].subscribe(...args);
+      this.__get<any>(property).subscribe(...args);
     } else {
       throw new Error(`this.component.instance does not have property ${property}`);
     }
@@ -130,14 +129,42 @@ export abstract class ComponentLoaderCommonAClass implements ComponentLoaderComm
   /**
    * Resolve component from `entryComponents`.
    * @protected
-   * @param {component} component Angular component.
+   * @template D Dynamic component type.
+   * @param {Type<D>} component
    * @returns {*}
-   * @memberof ComponentLoaderAClass
+   * @memberof ComponentLoaderCommonAClass
    */
-  protected __resolve(component: ComponentType<any>): any {
+  protected __resolve<D = T>(component: Type<D>): any {
     if (component) {
       return this.componentFactoryResolver.resolveComponentFactory(component);
     }
+  }
+
+  /**
+   * @protected
+   * @template S Source component type.
+   * @param {string[]} [p=this.__properties] Properties to be wrapped.
+   * @param {S} source Source component.
+   * @param {CallbackSetterType<S>} setter Callback function performed on set.
+   * @param {CallbackGetterType<S>} getter Callback function performed on get.
+   * @returns {this}
+   * @memberof ComponentLoaderCommonAClass
+   */
+  protected __wrap<S>(p: string[] = this.__properties, source: S, setter: CallbackSetterType<S>, getter: CallbackGetterType<S>): this {
+    this.connectClass = new ConnectClass(this.__prefix, this.__suffix);
+
+    if (this.connectClass instanceof ConnectClass) {
+      // Wrap properties with specified setter and getter.
+      this.connectClass.wrap<S>(source, p, setter, getter);
+
+      // Assign initial values to dynamic component.
+      p.forEach((property: string): void => {
+        if (this.connectClass) {
+          this.__set<any>(property, source[this.connectClass.propertyName(property)]);
+        }
+      });
+    }
+    return this;
   }
 }
 
