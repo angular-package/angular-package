ec03a0274113d7af85e8cff3b9590c8ed4de1c59
angularpackage <angular-package@wwwdev.io>
2018-03-14 16:52:27 +0100

    test: add component to test ComponentLoaderService

---
+++ packages/core/packages/component-loader/test/component-loader-service.component.ts
@@ -0,0 +1,62 @@
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+
+import { ComponentLoaderService } from './..';
+import { ComponentLoaderServiceInterface, ComponentLoaderConfigInterface, ComponentLoaderCommonInterface } from '../interface';
+import { DynamicComponent } from './dynamic.component';
+
+
+@Component({
+  template: '<div class="container"></div>',
+  providers: [
+    ComponentLoaderService
+  ]
+})
+export class ComponentLoaderServiceComponent
+  implements ComponentLoaderCommonInterface<DynamicComponent>, OnInit {
+  __prefix = '_';
+  __suffix = '_';
+
+  public model = {};
+  public key = 'defined';
+
+  constructor(public componentLoaderService: ComponentLoaderService<DynamicComponent>) { }
+
+  get __component() {
+    return this.componentLoaderService.__component;
+  }
+  __assign(p) {
+    this.componentLoaderService.__assign(p, this);
+  }
+  __connect(properties?: string[]): this {
+    this.componentLoaderService.__connect(properties, this);
+    return this;
+  }
+
+  __createSimple(): ComponentLoaderService<DynamicComponent> {
+    return this.componentLoaderService.init({
+      component: DynamicComponent,
+      container: '.container',
+    });
+  }
+
+  __create(): this {
+    this.componentLoaderService.init({
+      component: DynamicComponent,
+      componentPropertyName: '__component__', // <-- optional
+      container: '.container',
+      properties: [ 'key', 'model' ],
+      prefix: this.__prefix, //  <-- optional
+      suffix: this.__suffix // <-- optional
+    }, this);
+    return this;
+  }
+
+  __destroy() {
+    this.componentLoaderService.__destroy();
+  }
+  __subscribe(property, ...args): void {
+    this.componentLoaderService.__subscribe(property, ...args);
+  }
+
+  ngOnInit() { }
+}
