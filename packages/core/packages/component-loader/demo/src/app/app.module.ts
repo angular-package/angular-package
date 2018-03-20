eeef38e420f843fa2010d90b7b68d734b01635d0
angularpackage <angular-package@wwwdev.io>
2018-03-20 13:48:05 +0100

    refactor: apply angular-package style

--- packages/core/packages/component-loader/demo/src/app/app.module.ts
+++ packages/core/packages/component-loader/demo/src/app/app.module.ts
@@ -1,28 +1,54 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // added
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { DynamicComponent } from './dynamic/dynamic.component';
-// simport { DynamicTesterComponent } from './dynamic-tester/dynamic-tester.component';
-import { LoaderServiceComponent } from './loader-service/loader-service.component';
-import { LoaderClassComponent } from './loader-class/loader-class.component';
+import { AppLayoutComponent } from './app.layout.component';
+
+// @angular-package/core/component-loader
+import { DynamicComponent } from './core/component-loader/dynamic/dynamic.component';
+import { LoaderClassComponent } from './core/component-loader/loader-class/loader-class.component';
+import { LoaderServiceComponent } from './core/component-loader/loader-service/loader-service.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DynamicComponent,
-    // DynamicTesterComponent,
-    LoaderServiceComponent,
-    LoaderClassComponent
-  ],
   entryComponents: [
     DynamicComponent
   ],
+  declarations: [
+    AppComponent,
+    AppLayoutComponent,
+    DynamicComponent,
+    LoaderClassComponent,
+    LoaderServiceComponent
+  ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+
+    // @angular/material
+    MatButtonModule, // added
+    MatCheckboxModule, // added
+    MatIconModule, // added
+    MatInputModule, // added
+    MatRadioModule, // added
+    MatSidenavModule, // added
+    MatToolbarModule, // added
+    MatTooltipModule // added
   ],
   providers: [],
   bootstrap: [AppComponent]
