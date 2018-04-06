b20b5a274022722e314365450338ef25a187a2da
angularpackage <angular-package@wwwdev.io>
2018-04-06 11:44:04 +0200

    refactor(index): change ApChangeDetection to ApChangeDetector.

--- packages/change-detection/packages/interface/src/index.ts
+++ packages/change-detection/packages/interface/src/index.ts
@@ -1,3 +1,4 @@
-export { ApChangeDetection } from './interface';
+export { ApChangeDetector } from './change-detector.interface';
 export { ApChangeDetectionConfig } from './config.interface';
 export { ApChangeDetectionProperties } from './properties.interface';
+export { ApChangeDetectionWrap } from './wrap.interface';
