537b650d6eef3085e16f916169ebfd5a6010c4cf
angularpackage <angular-package@wwwdev.io>
2018-04-06 12:10:05 +0200

    refactor(detectToSetter): remove Function and Interface suffix, and argument, update setter.
    - Remove propertiesStoreName argument.
    - Update setter to use changeDetector instead of _detect, because of possibility to unactive method.

--- packages/change-detection/packages/src/core/detect-to-setter.func.ts
+++ packages/change-detection/packages/src/core/detect-to-setter.func.ts
@@ -3,48 +3,62 @@ import * as _ from 'lodash-es';
 
 // @angular-package
 import { StoreOriginalClass } from '@angular-package/core/store';
-import { ApPropertiesInterface } from '..';
+
+// internal
+import { ApChangeDetectionProperties } from '../../interface';
+import { ApChangeDetectorClass } from '../../change-detector';
 
 /**
+ * @export
  * @template T
  * @param {StoreOriginalClass} store
  * @param {Function} component
- * @param {ApPropertiesInterface} properties
+ * @param {ApChangeDetectionProperties} properties
  */
-export const detectToSetterFunction = function<T>(
+export function detectToSetter<T>(
   store: StoreOriginalClass,
   component: Function,
-  properties: ApPropertiesInterface,
-  propertiesStoreName: string
+  properties: ApChangeDetectionProperties
 ): void {
-  // Store target.prototype original setter and getter properties.
-  store.setterGetter(component, Object.keys(properties));
+  if (properties) {
 
-  // Detect changes on indicated properties by using setter.
-  _.each(Object.keys(properties), (property: string): void => {
-    Object.defineProperty(component.prototype, property, {
+    const propertiesKeys = Object.keys(properties);
 
-      // Update setter.
-      set: function (value: any) {
-        if (store.setter[property] !== undefined) {
-          store.setter[property].apply(this, arguments);
-        } else {
-          this[`_${property}`] = value;
-        }
-        // Detect changes if indicated property is set to true.
-        if (this[`_${propertiesStoreName}`][property] === true) {
-          this['_detect']();
-        }
-      },
+    // Store target.prototype original setter and getter properties.
+    store.setterGetter(component, propertiesKeys);
 
-      // Update getter.
-      get: function () {
-        if (store.getter[property]) {
-          return store.getter[property].apply(this, arguments);
-        } else {
-          return this[`_${property}`];
-        }
+    // Detect changes on indicated properties by using setter.
+    _.each(propertiesKeys, (property: string): void => {
+
+      // Create wrapped property if getter is not set.
+      if (store.getter[property] === undefined) {
+        Object.defineProperty(component.prototype, `_${property}`, { writable: true });
       }
+
+      // Update setter and getter.
+      Object.defineProperty(component.prototype, property, {
+
+        // Update setter.
+        set: function (value: any) {
+          if (store.setter[property] !== undefined) {
+            store.setter[property].apply(this, arguments);
+          } else {
+            this[`_${property}`] = value;
+          }
+          // Detect changes if indicated property is set to true.
+          this['changeDetector'].detect(this, property);
+        },
+
+        // Update getter.
+        get: function () {
+          if (store.getter[property]) {
+            return store.getter[property].apply(this, arguments);
+          } else {
+            return this[`_${property}`];
+          }
+        }
+
+      });
     });
-  });
-};
+  }
+}
