eeb196e9e20c3b013686ffd5d532685838223f72
angularpackage <angular-package@wwwdev.io>
2018-04-10 18:40:30 +0200

    feat(WRAP_DEFAULT): Add default options.

---
+++ packages/change-detection/packages/src/wrap.default.ts
@@ -0,0 +1,8 @@
+import { ApChangeDetectionOptions } from '../interface';
+
+export const WRAP_DEFAULT: ApChangeDetectionOptions = {
+  detach: '_detach',
+  detect: '_detect',
+  properties: '_properties',
+  reattach: '_reattach'
+};
