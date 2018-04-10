374ebccafee6694333a564b3a73b733a84a7d9bc
angularpackage <angular-package@wwwdev.io>
2018-04-10 18:50:09 +0200

    refactor(OPTIONS): Change WRAP_DEFAULT to DEFAULT_OPTIONS.

--- packages/change-detection/packages/src/default_options.ts
+++ packages/change-detection/packages/src/default_options.ts
@@ -1,6 +1,6 @@
 import { ApChangeDetectionOptions } from '../interface';
 
-export const WRAP_DEFAULT: ApChangeDetectionOptions = {
+export const DEFAULT_OPTIONS: ApChangeDetectionOptions = {
   detach: '_detach',
   detect: '_detect',
   properties: '_properties',
