84ee2138fe684a123872d06b8bced10875a1d139
angularpackage <angular-package@wwwdev.io>
2018-04-10 19:05:36 +0200

    refactor(ApChangeDetectorClass): Crucial update.
    - Replace Type<T> to T.
    - Import `each` from `lodash-es.
    - Property `cd` access modifier is now public.
    - Detection now can be undefined.
    - Add property `propertyWrapper` with `PropertyWrapperClass()` instance.
    - New method `detectToSetter()` that use `PropertyWrapperClass()`.
    - When use method `detach()`, `detectToSetter()` method is invoked.
    - New method `setDetection()` to change component change detection state.
    - `find()` method now returns `this` object.
    - Methods `reattach()`, `detach()` change `detection` respectively true and false.

--- packages/change-detection/packages/change-detector/src/change-detector.class.ts
+++ packages/change-detection/packages/change-detector/src/change-detector.class.ts
@@ -1,9 +1,12 @@
-import { ChangeDetectorRef, Type } from '@angular/core';
-import * as _ from 'lodash-es';
+// external
+import { ChangeDetectorRef, } from '@angular/core';
+import { each } from 'lodash-es';
+import { StoreOriginalClass } from '@angular-package/core/store';
 
 // internal
 import { instanceOf } from '../../src/instance-of.func';
 import { ApChangeDetectionProperties } from '../../interface';
+import { PropertyWrapperClass } from '@angular-package/core/property-wrapper';
 
 /**
  * Find `ChangeDetectorRef` component instance and simply use it.
@@ -13,40 +16,70 @@ import { ApChangeDetectionProperties } from '../../interface';
  */
 export class ApChangeDetectorClass<T> {
 
-  // Change detector instance property name.
-  private cd?: string;
-  public detection = false;
+  /**
+   * Property name of found `ChangeDetectorRef` instance.
+   * @type {string}
+   * @memberof ApChangeDetectorClass
+   */
+  public cd?: string;
+
+  /**
+   * Whether detection is on or off.
+   * @type {boolean}
+   * @memberof ApChangeDetectorClass
+   */
+  public detection?: boolean;
+
+  /**
+   * Detect changes when specified property is true.
+   * @type {ApChangeDetectionProperties}
+   * @memberof ApChangeDetectorClass
+   */
   public properties?: ApChangeDetectionProperties;
 
   /**
-   * Creates an instance of ApChangeDetectorClass.
-   * @param {Type<T>} component
+   * Add `detect()` method to all properties.
+   * @type {PropertyWrapperClass}
    * @memberof ApChangeDetectorClass
    */
-  constructor(component: Type<T>) {
-    this.find(component);
+  public propertyWrapper: PropertyWrapperClass = new PropertyWrapperClass();
+
+  /**
+   * Creates an instance of ApChangeDetectorClass.
+   * @param {T} component It is used to find `ChangeDetectorRef` instance.
+   * @memberof ApChangeDetectorClass
+   */
+  constructor(component: T) {
+    this.find(component).setDetection(component);
+    return this;
   }
 
   /**
-   * Detach component from detection tree.
-   * @param {Type<T>} component
+   * Detaches component change detector from the change detector tree.
+   * The detached change detector will not be checked until it is reattached.
+   * @param {T} component Used to invoke `ChangeDetectorRef` methods by using `cd` property, in this case `detach()`.
+   * @returns {this} ApChangeDetectorClass.
    * @memberof ApChangeDetectorClass
    */
-  public detach(component: Type<T>): void {
+  public detach(component: T): this {
     setTimeout(() => {
       if (this.cd) {
         component[this.cd].detach();
       }
     }, 0);
+    this.detectToSetter(component);
+    this.detection = false;
+    return this;
   }
 
   /**
-   * Detect changes in specified component, and when provided property is true in properties.
-   * @param {Type<T>} component
-   * @param {string} [property]
+   * Detect changes in specified component, and conditionally by providing property name.
+   * @param {T} component Object to invoke `ChangeDetectorRef` methods by using `cd` property, in this case `detectChanges()`.
+   * @param {string} [property] Name of property found in `properties` as true to invoke `detectChanges()`.
+   * @returns {this} ApChangeDetectorClass.
    * @memberof ApChangeDetectorClass
    */
-  public detect(component: Type<T>, property?: string): void {
+  public detect(component: T, property?: string): this {
     if (this.cd) {
       if (property) {
         if (this.properties && this.properties[property] === true) {
@@ -56,31 +89,74 @@ export class ApChangeDetectorClass<T> {
         component[this.cd].detectChanges();
       }
     }
+    return this;
   }
 
   /**
-   * Reattach component to detection.
-   * @param {Type<T>} component
+   * All indicated properties will have added method `detect()` to setter.
+   * @param {T} component Object where properties are gonna be changed.
+   * @returns {this}
    * @memberof ApChangeDetectorClass
    */
-  public reattach(component: Type<T>): void {
+  public detectToSetter(component: T): this {
+    if (instanceOf<ApChangeDetectionProperties>(this, 'properties') && this.properties) {
+      this.propertyWrapper.wrap<T>(
+        component,
+        Object.keys(this.properties),
+        (property: string, sourcePropertyName: string, source?: T): void => {
+          if (source) {
+            source['changeDetector'].detect(component, property);
+          }
+        },
+        property => undefined
+      );
+    }
+    return this;
+  }
+
+  /**
+   * Reattach component change detector to the change detector tree.
+   * @param {T} component Used to invoke `ChangeDetectorRef` methods by using `cd` property, in this case `reattach()`.
+   * @returns {this} ApChangeDetectorClass.
+   * @memberof ApChangeDetectorClass
+   */
+  public reattach(component: T): this {
     setTimeout(() => {
       if (this.cd) {
         component[this.cd].reattach();
       }
     }, 0);
+    this.detection = true;
+    return this;
+  }
+
+  /**
+   * Detach or reattach component depends on `detection` property.
+   * @param {T} component Component in which change detector tree will be modified.
+   * @returns {this}
+   * @memberof ApChangeDetectorClass
+   */
+  public setDetection(component: T): this {
+    if (component) {
+      if (this.detection === true) {
+        this.reattach(component);
+      } else if (this.detection === false) {
+        this.detach(component);
+      }
+    }
+    return this;
   }
 
   /**
    * Search for change detector instance in specified component and return its key.
    * @private
-   * @param {Type<T>} component
-   * @returns {string}
+   * @param {T} component To find `ChangeDetectorRef` instance.
+   * @returns {this}
    * @memberof ApChangeDetectorClass
    */
-  private find(component: Type<T>): string {
+  private find(component: T): this {
     if (this.cd === undefined) {
-      _.each(component, (ChangeDetectorRefInstance: ChangeDetectorRef, key: string) => {
+      each(component, (ChangeDetectorRefInstance: ChangeDetectorRef, key: string) => {
         if (component[key] instanceof Object) {
           if (instanceOf<ChangeDetectorRef>(component[key], 'detectChanges')) {
             if (component[key].detectChanges instanceof Function) {
@@ -97,6 +173,6 @@ export class ApChangeDetectorClass<T> {
         `);
       }
     }
-    return this.cd;
+    return this;
   }
 }
