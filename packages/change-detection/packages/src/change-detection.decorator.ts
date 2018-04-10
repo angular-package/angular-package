d740a19ffdd57a6ea4f4d33403fd3a7edcd89af1
angularpackage <angular-package@wwwdev.io>
2018-04-10 18:57:22 +0200

    refactor(ApChangeDetection): Use `PropertyWrapperClass`, general update.
    - Remove unnecessary imports.
    - Change interface ApChangeDetectionConfig to ApChangeDetectionProperties.
    - Remove method detectToSetter and use instead PropertyWrapperClass.

--- packages/change-detection/packages/src/change-detection.decorator.ts
+++ packages/change-detection/packages/src/change-detection.decorator.ts
@@ -1,29 +1,38 @@
-// external
-import { Injectable } from '@angular/core';
-import * as _ from 'lodash-es';
-
 // @angular-package
-import { CycleHookType } from '@angular-package/core/type';
-import { CycleHookInterface } from '@angular-package/core/interface';
 import { StoreOriginalClass } from '@angular-package/core/store';
+import { PropertyWrapperClass } from '@angular-package/core/property-wrapper';
 
 // internal
 import { configureDetector, detectToSetter } from './core';
-import { ApChangeDetectionConfig, ApChangeDetectionProperties } from '../interface';
+import { ApChangeDetectionOptions, ApChangeDetectionProperties } from '../interface';
 
 /**
  * Indicate component properties by name whether to detect or not changes on them.
  * @export
  * @template T
  * @param {ApChangeDetectionProperties} properties
- * @param {ApChangeDetectionConfig} [config]
+ * @param {ApChangeDetectionOptions} [config]
  * @returns {Function}
  */
-export function ApChangeDetection<T>(properties: ApChangeDetectionProperties, config?: ApChangeDetectionConfig): Function {
+export function ApChangeDetection<T>(properties: ApChangeDetectionProperties, config?: ApChangeDetectionOptions): Function {
   return function (component: Function): void {
+    const wrapper: PropertyWrapperClass = new PropertyWrapperClass();
     const store = new StoreOriginalClass();
+
     configureDetector<T>(component, properties, config);
-    detectToSetter<T>(store, component, properties);
+
+    if (properties) {
+      wrapper.wrap(
+        component,
+        Object.keys(properties),
+        (property: string, sourcePropertyName: string, source?: T): void => {
+          if (source) {
+            source['changeDetector'].detect(source, property);
+          }
+        },
+        (property: string) => undefined
+      );
+    }
   };
 }
 
