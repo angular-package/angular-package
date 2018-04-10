ee33cd5d8711683477f443624cefcd79890e76d3
angularpackage <angular-package@wwwdev.io>
2018-04-10 18:42:17 +0200

    refactor(ApChangeDetectionOptions): change name from ApChangeDetectionConfig.

---
+++ packages/change-detection/packages/interface/src/options.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * Change features property names.
+ * @export
+ * @interface ApChangeDetectionOptions
+ */
+export interface ApChangeDetectionOptions {
+  detach?: string;
+  detect?: string;
+  properties?: string;
+  reattach?: string;
+}
