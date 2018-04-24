0f48de9f0d3b849f1dcb2854f4d197a47cd481f5
angularpackage <angular-package@wwwdev.io>
2018-04-24 14:24:20 +0200

    refactor: Update path to index.d.ts.

--- packages/prism/packages/core/src/prism.service.ts
+++ packages/prism/packages/core/src/prism.service.ts
@@ -1,4 +1,4 @@
-/// <reference path="./../../typings/index.d.ts" />
+/// <reference path="./../../../typings/index.d.ts" />
 
 // external
 import { ElementRef, Injectable, SecurityContext } from '@angular/core';
