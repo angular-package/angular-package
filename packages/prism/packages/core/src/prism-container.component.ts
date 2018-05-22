988b308bd539d71e13e41edabe4f465255c1d89b
angularpackage <angular-package@wwwdev.io>
2018-05-22 13:53:23 +0200

    feat(PrismContainerComponent): Add stateless component.

---
+++ packages/prism/packages/core/src/prism-container.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ap-prism-container',
+  templateUrl: './prism-container.component.html'
+})
+export class PrismContainerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    // console.info(this);
+  }
+}
