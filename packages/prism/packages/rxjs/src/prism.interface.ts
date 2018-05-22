6034073b4eb691b03cf9555274b748ed9e95a50b
angularpackage <angular-package@wwwdev.io>
2018-05-22 13:38:55 +0200

    refactor(PrismInterface): Update interface, remove import.
    - Removed ElementRef, Subscription import.
    - All properties from interfaces are optional.
    - Changed undefined to ?.

--- packages/prism/packages/rxjs/src/prism.interface.ts
+++ packages/prism/packages/rxjs/src/prism.interface.ts
@@ -1,14 +1,10 @@
-// external
-import { ElementRef } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-
 // internal
 import { CallbackType, SanitizedType } from './prism.type';
 
 export interface PrismInterface {
-  async: boolean;
-  callback?: CallbackType | undefined;
-  code?: SanitizedType | undefined;
-  language: string;
-  interpolation: Object | undefined;
+  async?: boolean;
+  callback?: CallbackType;
+  code?: SanitizedType;
+  language?: string;
+  interpolation?: Object;
 }
