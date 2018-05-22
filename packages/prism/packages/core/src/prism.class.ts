9759440948a7a6b299e9b67ed99fe9e55d4f7076
angularpackage <angular-package@wwwdev.io>
2018-05-22 14:32:32 +0200

    refactor(PrismAClass): General update.
    - Added `ComponentLoader` to dynamic load stateless component.
    - Added `ApLinkProperyWithService` to link respectively properties from component with service.
    - `ApChangeDetection` to optimize performance of component.
    
    - Removed `lodash-es`.
    - Changed class name from `PrismHoodClass` to `PrismAClass`.
    - Removed `PrismInterface` implements.
    - Added implements `ApChangeDetector` .
    - Added implements `ComponentLoaderCommonInterface` for future dynamic component loading.
    - Added `@ViewChild` of `preElement` and `codeElement`.
    - Removed `cd` property, now it is `_properties`.
    - Added `attribute` and `class` Input to add new to <pre> and <code> .
    - Setters and Getters went to `ApLinkProperyWithService`.
    - Removed `highlightElement()` method - all logic is now in service.
    - Added `_detach()`, `_detect()`, `_reattach()` method because of `ApChangeDetection` feature.
    - Added `attributeHandlerService: ApAttributeHandlerService` to handle HTMLElement attributes.
    - Added `classNameHandlerService: ApClassnameHandlerService` to handle HTMLElement classes.
    - Store last `class` and `attribute` value.

--- packages/prism/packages/core/src/prism.class.ts
+++ packages/prism/packages/core/src/prism.class.ts
@@ -1,162 +1,286 @@
 /// <reference path="./../../../typings/index.d.ts" />
-
 // external
 import {
   ChangeDetectorRef,
   ElementRef,
   Input,
   Injectable,
-  ViewChild
+  Renderer,
+  ViewChild,
+  Type
 } from '@angular/core';
-import * as _ from 'lodash-es';
+
+// @angular-package/change-detection
+import { ApChangeDetectionProperties, ApChangeDetector } from '@angular-package/change-detection/interface';
+import { ApChangeDetectorClass } from '@angular-package/change-detection/change-detector';
+import { ComponentLoaderService } from '@angular-package/core/component-loader';
+import { ComponentLoaderCommonInterface } from '@angular-package/core/component-loader/interface';
 
 // internal
-import { PrismInterface } from './prism.interface';
-import { CallbackType } from './prism.type';
+/// interface
+import { ApPrismTemplate } from '../interface';
+import { ApObject } from '../interface/src/object.interface';
+
+/// type
+import { CallbackType } from '../type/src/prism.type';
+
+/// service
 import { PrismService } from './prism.service';
+import { ApAttributeHandlerService } from '../../src/attribute-handler.service';
+import { ApClassnameHandlerService } from '../../src/classname-handler.service';
+
+import { ApPrismComponent } from '.';
+import { PrismContainerComponent } from './prism-container.component';
+
+export interface ApPrismProperties {
+  async?: boolean;
+  attribute?: boolean;
+  callback?: boolean;
+  class?: boolean;
+  code?: boolean;
+  hooks?: boolean;
+  interpolation?: boolean;
+  language?: boolean;
+}
 
 /**
  * @export
  * @abstract
- * @class PrismHoodClass
+ * @class PrismAClass
+ * @implements {ApChangeDetector<ApPrismComponent>}
  */
 @Injectable()
-export abstract class PrismHoodClass implements PrismInterface {
-  @ViewChild('el', { read: ElementRef }) el: ElementRef;
+export
+  abstract class PrismAClass
+  implements ApChangeDetector<ApPrismComponent>, ComponentLoaderCommonInterface<ApPrismComponent> {
 
-  ready = false;
+    // __component?: ComponentRef<T>;
+    // __componentPropertyName?: string;
+    __create?: <D = PrismContainerComponent>(component: Type<D>) => this;
+    __destroy?: () => any;
+    // __properties?: string[];
+    // __prefix?: string;
+    // __suffix?: string;
+    __assign?: <S>(p: string | string[], source: S) => void;
+    __set?: <PT>(property: string, value: PT) => void;
+    __get?: <PT>(property: string) => PT | undefined;
+    __subscribe?: (property: string, ...args: any[]) => void;
 
-  __properties: any;
+  /**
+   * @type {ElementRef}
+   * @memberof PrismAClass
+   */
+  @ViewChild('codeElement', { read: ElementRef }) codeElement?: ElementRef;
 
-  _cd: any;
-  @Input('cd')
-  set cd(cd: any) {
-    this._cd = cd;
-    if (this.ready === true) {
-      this.__properties = cd;
-    }
-  }
-  get cd() {
-    return this._cd;
-  }
+  /**
+   * @readme
+   * @type {ElementRef}
+   * @memberof PrismAClass
+   */
+  @ViewChild('preElement', { read: ElementRef }) preElement?: ElementRef;
 
   /**
    * Whether to use Web Workers to improve performance and avoid blocking the UI when highlighting very large chunks of code.
    * False by default (why? - http://prismjs.com/faq.html#why-is-asynchronous-highlighting-disabled-by-default).
+   * @readme
    */
-  public _async = false;
-  @Input('async')
-  set async(async: boolean) {
-    this._async = async;
+  @Input() async?: boolean;
+
+  /**
+   * Store last attribute value.
+   * @tested
+   * @type {(ApPrismTemplate<ApObject<string>> | null)}
+   * @memberof PrismAClass
+   */
+  lastAttribute: ApPrismTemplate<ApObject<string>> | null = null;
+
+  /**
+   * To add or remove the specified list of attributes with `string` value to `pre`, `code` HTMLElement.
+   * @readme
+   * @tested #9
+   * @type {(ApPrismTemplate<ApObject<string>> | null)}
+   * @memberof PrismAClass
+   */
+  @Input('attribute') set attribute(attribute: ApPrismTemplate<ApObject<string>> | null) {
+    if (attribute) {
+
+      // <pre> attributes.
+      if (this.preElement && attribute.pre) {
+        this.attributeHandlerService.set(
+          this.preElement,
+          attribute.pre,
+          (this.lastAttribute && this.lastAttribute.pre) ? this.lastAttribute.pre : undefined
+        );
+      }
+
+      // <code> attributes.
+      if (this.codeElement && attribute.code) {  
+        this.attributeHandlerService.set(
+          this.codeElement,
+          attribute.code,
+          (this.lastAttribute && this.lastAttribute.code) ? this.lastAttribute.code : undefined
+        );
+      }  
+    }
+    this.lastAttribute = Object.assign({}, attribute);
   }
-  get async(): boolean {
-    return this._async;
+
+  /**
+   * Store last class value.
+   * @tested
+   * @type {(ApPrismTemplate<string[]> | null)}
+   * @memberof PrismAClass
+   */
+  lastClassValue: ApPrismTemplate<string[]> | null = null;
+
+  /**
+   * 
+   * @readme
+   * @tested #10
+   * @type {(ApPrismTemplate<string[]> | null)}
+   * @memberof PrismAClass
+   */
+  @Input('class') set class(classValue: ApPrismTemplate<string[]> | null) {
+    if (classValue) {
+
+      // <pre> class.
+      if (this.preElement && classValue.pre) {
+        this.classNameHandlerService.set(
+          this.preElement,
+          classValue.pre,
+          (this.lastClassValue && this.lastClassValue.pre) ? this.lastClassValue.pre : undefined );
+      }
+
+      // <code> class.
+      if (this.codeElement && classValue.code) {
+        this.classNameHandlerService.set(
+          this.codeElement,
+          classValue.code,
+          (this.lastClassValue && this.lastClassValue.code) ? this.lastClassValue.code : undefined );
+      }
+    }
+    this.lastClassValue = Object.assign({}, classValue);
   }
 
   /**
    * An optional callback to be invoked after the highlighting is done.
    * Mostly useful when async is true, since in that case, the highlighting is done asynchronously.
-   * @memberof PrismHoodClass
+   * @readme
+   * @tested
+   * @type {CallbackType}
+   * @memberof PrismAClass
    */
-  public _callback: CallbackType | undefined;
-  @Input('callback')
-  set callback(callback: CallbackType | undefined) {
-    this._callback = callback;
-  }
-  get callback(): CallbackType | undefined {
-    return this._callback;
-  }
+  @Input() callback: CallbackType | undefined;
 
   /**
    * A string with the code to be highlighted.
+   * @readme
+   * @tested #13
    * @type {string}
-   * @memberof PrismHoodClass
+   * @memberof PrismAClass
    */
-  public _code: string;
-  @Input('code')
-  set code(code: string) {
-    this._code = code;
-    if (this.ready) {
-      if (this.__properties.code === true) {
-        this.highlightElement({ code, language: this.language });
-      }
-    }
-  }
-  get code(): string {
-    return this._code;
-  }
+  @Input() code?: string;
 
   /**
+   * @readme
    * @type {Object}
-   * @memberof PrismHoodClass
+   * @memberof PrismAClass
    */
-  public _hooks: Object;
-  @Input('hooks')
-  set hooks(hooks: Object) {
-    this._hooks = hooks;
-    if (hooks instanceof Object) {
-      _.forEach(hooks, (element: any, key: string) => {
-        this.prismService.hooks().add(key, element);
-      });
-    }
-    this.highlightElement({ code: this.code, language: this.language });
-  }
-  get hooks(): Object {
-    return this._hooks;
-  }
+  @Input() hooks?: Object;
 
   /**
    * Valid language identifier.
+   * @tested #3 #4 #5
    * @type {string}
-   * @memberof PrismHoodClass
+   * @memberof PrismAClass
    */
-  public _language: string;
-  @Input('language') set language(language: string) {
-    if (language) {
-      if (typeof (language) === 'string') {
-        this._language = language;
-        this.highlightElement({ code: this.code, language });
-      } else {
-        throw new Error(`Property \`language\` should be \`string\` instead of provided \`${typeof (language)}\``);
-      }
-    } else {
-      throw new Error('Missing property `language`.');
-    }
+  @Input() language?: string;
+
+  /**
+   * @tested #7 #8 #14
+   * @type {ApChangeDetectionProperties}
+   * @memberof PrismAClass
+   */
+  _properties?: ApChangeDetectionProperties;
+  @Input('properties')
+  set properties(properties: ApChangeDetectionProperties | undefined) {
+    this._properties = Object.assign({}, this._properties, properties);
+    this.prismService.properties = this._properties;
   }
-  get language(): string {
-    return this._language;
+  get properties(): ApChangeDetectionProperties | undefined {
+    return this.prismService.properties;
   }
+
+  /**
+   * @tested #6
+   * @type {ApChangeDetectorClass<ApPrismComponent>}
+   * @memberof PrismAClass
+   */
+  changeDetector?: ApChangeDetectorClass<ApPrismComponent>;
+
+  /** 
+   * @tested #8
+   * @memberof PrismAClass
+   */
+  @Input() detection = false; // <--- Required, initialize detection with specified value true or false.
 
   /**
    * Object properties to interpolate.
-   * @type {(Object | undefined)}
-   * @memberof PrismHoodClass
+   * @tested #15
+   * @type {Object}
+   * @memberof PrismAClass
    */
-  @Input('interpolation') public interpolation?: Object | undefined;
+  @Input() interpolation?: Object;
 
   /**
-   * Creates an instance of PrismHoodClass.
+   * @tested
+   * @type {boolean}
+   * @memberof PrismAClass
+   */
+  ready?: boolean;
+
+  /**
+   * Creates an instance of PrismAClass.
+   * @param {ApAttributeHandlerService} attributeHandlerService
+   * @param {ApClassnameHandlerService} classNameHandlerService
    * @param {ChangeDetectorRef} changeDetectorRef
    * @param {PrismService} prismService
-   * @memberof PrismHoodClass
+   * @param {Renderer} renderer
+   * @memberof PrismAClass
    */
   constructor(
+    public attributeHandlerService: ApAttributeHandlerService,
+    public classNameHandlerService: ApClassnameHandlerService,
+    public componentLoaderService: ComponentLoaderService<ApPrismComponent>,
     public changeDetectorRef: ChangeDetectorRef,
-    public prismService: PrismService
-  ) {}
+    public prismService: PrismService,
+    public renderer: Renderer
+  ) { }
 
   /**
-   * @param {{code: string, language: string}} result
-   * @memberof PrismHoodClass
+   * ChangeDetector method.
+   * @readme
+   * @tested #16
+   * @package @angular-package/change-detection
+   * @memberof PrismAClass
    */
-  highlightElement(result: { code: string, language: string }): void {
-    if (this.ready === true) {
-      this.prismService.highlight(this.el, {
-        async: this.async,
-        callback: this.callback,
-        code: result.code,
-        interpolation: this.interpolation
-      });
-    }
-  }
+  _detach(): void { }
+
+  /**
+   * ChangeDetector method.
+   * @readme
+   * @tested #17
+   * @package @angular-package/change-detection
+   * @memberof PrismAClass
+   */
+  _detect(): void { }
+
+  /**
+   * ChangeDetector method.
+   * @readme
+   * @tested #18
+   * @package @angular-package/change-detection
+   * @memberof PrismAClass
+   */
+  _reattach(): void { }
 }
