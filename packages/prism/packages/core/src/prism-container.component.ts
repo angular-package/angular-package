1e5bc4cb74a00400b4b3a23a59d24359c9d925a0
angularpackage <angular-package@wwwdev.io>
2018-05-28 11:36:49 +0200

    refactor(PrismContainerComponent): Add extends with PRismInputClass, jsdoc.

--- packages/prism/packages/core/src/prism-container.component.ts
+++ packages/prism/packages/core/src/prism-container.component.ts
@@ -1,11 +1,23 @@
+// external
 import { Component, OnInit } from '@angular/core';
 
+// internal
+import { PrismInputClass } from './prism-input.class';
+
+/**
+ * @export
+ * @class PrismContainerComponent
+ * @extends {PrismInputClass}
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'ap-prism-container',
   templateUrl: './prism-container.component.html'
 })
-export class PrismContainerComponent implements OnInit {
-  constructor() { }
+export
+  class PrismContainerComponent
+  extends PrismInputClass
+  implements OnInit {
 
   ngOnInit(): void {
     // console.info(this);
