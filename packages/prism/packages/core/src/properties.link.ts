43643dd32ef25755660500fc5907d13aefb32924
angularpackage <angular-package@wwwdev.io>
2018-05-28 11:39:12 +0200

    refactor(PROPERTIES_LINK): Move const to separate file.

--- packages/prism/packages/core/src/properties.link.ts
+++ packages/prism/packages/core/src/properties.link.ts
@@ -1,12 +1,18 @@
 
 export const PROPERTIES_LINK = [
   'async',
+  'attribute',
+  'attributeHandlerService',
+  'classNameHandlerService',
   'callback',
+  'class',
   'code',
   'codeElement',
   'hooks',
   'interpolation',
   'language',
   'preElement',
+  // TODO: Change PropertyWrapperClass to use properties.
+  // 'properties',
   'ready'
 ];
