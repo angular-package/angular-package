b3d08ffb228f3167a4469088a3bd992c7dc5a27d
angularpackage <angular-package@wwwdev.io>
2018-05-28 14:38:37 +0200

    refactor(AppComponent): Add properties to demo, add descriptions, use plugin line-numbers.
    - Added attribute, class properties.
    - Added descriptions to language, content, class, attribute.
    - Imported line-numbers plugin.
    - Used ApPrismTemplate, ApObject for attribute and class property.

--- packages/prism/demo/src/app/app.component.ts
+++ packages/prism/demo/src/app/app.component.ts
@@ -1,5 +1,10 @@
 // app.component.html
 import { Component } from '@angular/core';
+import { ApPrismTemplate } from '@angular-package/prism/core/interface';  // <----- Import interface to configure code or pre tag.
+import { ApObject } from '@angular-package/prism/core/interface/src/object.interface';  // <----- Import interface for objects.
+
+// Import line-numbers prism plugin.
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';  // <----- Import `line-numbers` plugin.
 
 @Component({
   selector: 'app-root',
@@ -7,10 +12,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  attribute: ApPrismTemplate<ApObject<string>> = {  // <----- Add `data-start` attribue to `<pre>` tag.
+    pre: {
+      'data-start': '-5'
+    }
+  };
+  class: ApPrismTemplate<string[]> = {              // <----- `class` property to add class to `<pre`> tag.
+    pre: ['line-numbers']
+  };
   title = 'app';
-  language = 'html';                          // <----- Add property with language.
-  content = '<p>test {{language}}</p>';       // <----- HTML content to be highlighted by prism.
+  language = 'html';                                // <----- Add property with language.
+  content = '<p>test {{language}}</p>';             // <----- HTML content to be highlighted by prism.
   interpolate = {
-    language: this.language                   // <----- Interpolate property `language` in `content`.
+    language: this.language                         // <----- Interpolate property `language` in `content`.
   };
 }
