46e7b542482850d50efadc04728f74c9a9b35456
angularpackage <angular-package@wwwdev.io>
2018-05-28 14:20:36 +0200

    refactor(ApLinkProperyWithService): Update parameter name, add instance, change conditions.

--- packages/prism/packages/src/property.decorator.ts
+++ packages/prism/packages/src/property.decorator.ts
@@ -1,18 +1,34 @@
 import { PropertyWrapperClass } from '@angular-package/core/property-wrapper';
 
-export function ApLinkProperyWithService<S>(properties: string[], service: string): Function {
+export function ApLinkProperyWithService<S>(properties: string[], serviceName: string, debug = false): Function {
+  const propertyClass = new PropertyWrapperClass();
   return function (component: S): void {
-    new PropertyWrapperClass().wrap<S>(
-      component,
-      properties,
+    // console.info(component, properties, service);
+    if (debug) {
+
+    }
+    propertyClass.wrap<S>(component, properties,
       (property: string, sourcePropertyName: string, source?: S) => {
-        if (source && service) {
-          source[service][property] = source[sourcePropertyName];
+        if (source && !source[sourcePropertyName]) {
+          // throw new Error('');
+        }
+        // this.prismService.properties = this._properties = { ...this._properties, ...properties };
+
+        if (property && sourcePropertyName && source && serviceName && source[serviceName] && source[sourcePropertyName]) {
+          /*
+          if (property === 'properties') {
+            source[serviceName][property] = source['_properties'] = { ...source['_properties'] , ...source[sourcePropertyName] };
+            console.info(property, source['_properties'], source[serviceName][property]);
+          } else {
+            source[serviceName][property] = source[sourcePropertyName];
+          }
+          */
+          source[serviceName][property] = source[sourcePropertyName];
         }
       },
       (property: string, source?: S) => {
-        if (source && source[service]) {
-          return source[service][property];
+        if (source && property && serviceName && source[serviceName]) {
+          return source[serviceName][property];
         }
       }
     );
