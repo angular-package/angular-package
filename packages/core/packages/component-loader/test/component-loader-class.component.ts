7622e3187d90572b2322e6d079ee73157024886f
angularpackage <angular-package@wwwdev.io>
2018-06-01 19:14:10 +0200

    test(ComponentLoaderClassComponent): New tslint rules.
    Used new tslint rules:
    - 'public' is implicit. (member-access)
    - Missing blank line before return (newline-before-return)
    - expected call-signature: 'create' to have a typedef (typedef)

--- packages/core/packages/component-loader/test/component-loader-class.component.ts
+++ packages/core/packages/component-loader/test/component-loader-class.component.ts
@@ -3,39 +3,38 @@ import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from
 import { ComponentLoaderClass } from './../src';
 import { DynamicComponent } from './dynamic.component';
 
-
 @Component({
   template: '<div #newcontainer></div>'
 })
 export class ComponentLoaderClassComponent extends ComponentLoaderClass<DynamicComponent> {
+  @ViewChild('newcontainer', { read: ViewContainerRef }) public container: any;
 
   __prefix = '_';
   __suffix = '_';
 
-  public model = {};
-  public key = 'defined';
+  model = {};
+  key = 'defined';
 
   _wrappedGet = false;
   _wrappedSet = false;
 
   // All logic can be placed here.
-  public _wrapped: boolean;
+  _wrapped: boolean;
   set wrapped(value: boolean) {
     this._wrappedSet = true;
     this._wrapped = value;
   }
   get wrapped(): boolean {
     this._wrappedGet = true;
+    
     return this._wrapped;
   }
-
-  @ViewChild('newcontainer', { read: ViewContainerRef }) public container: any;
 
   constructor(c: ComponentFactoryResolver) {
     super(c);
   }
 
-  public create() {
+  create(): void {
     this.__create(DynamicComponent);
   }
 }
