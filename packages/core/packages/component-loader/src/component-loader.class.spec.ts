306a4af9ecf6406fb606a48885bf9f0531b8ca19
angularpackage <angular-package@wwwdev.io>
2018-06-11 19:21:15 +0200

    test(ComponentLoaderClass): Move some jasmine settings to TestingClass.

--- packages/core/packages/component-loader/src/component-loader.class.spec.ts
+++ packages/core/packages/component-loader/src/component-loader.class.spec.ts
@@ -3,285 +3,253 @@ import {
 
 } from 'jasmine';
 
-// external
-import { 
-  // NO_ERRORS_SCHEMA, 
-  // ViewChild,
-  // ComponentRef,
-  // DebugElement
-} from '@angular/core';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-// import { By } from '@angular/platform-browser';
-import {
-  async,
-  // ComponentFixture,
-  TestBed
-  // inject, 
-} from '@angular/core/testing';
-
 // internal
 import { TestingClass } from '../../testing';
 import { DynamicComponent } from '../test/dynamic.component';
 import { ComponentLoaderClassComponent } from '../test/component-loader-class.component';
 import { TestModule } from '../test/test.module';
 
-beforeAll(() => {
-  TestBed.resetTestEnvironment();
-  TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+const testing: TestingClass<ComponentLoaderClassComponent> =
+  new TestingClass<ComponentLoaderClassComponent>('ComponentLoaderClass', {
+    imports: [TestModule]
+  }, ComponentLoaderClassComponent, {
+  console: true,
+  execute: [ ]
 });
 
-describe('ComponentLoaderClass', () => {
+/**
+ * Main tests.
+ */
+const MAIN_TESTS = {
+  '<div>': {
+    true: () => testing.selector('div')
+  },
+  '`__component` property undefined': {
+    true: () => testing.undefined('__component')
+  },
+  '`__component` property defined': {
+    true: () => testing
+      .before(component => component.__create(DynamicComponent))
+      .defined('__component')
+  },
+  'Dynamic component model should be defined': {
+    true: () => testing
+      .before(component => component.__create(DynamicComponent))
+      .defined('__component.instance.model')
+  }
+};
 
-  const testingClass: TestingClass<ComponentLoaderClassComponent> = new TestingClass<ComponentLoaderClassComponent>(undefined, {
-    console: true,
-    execute: []
-  });
+/**
+ * Method `__assign()`.
+ */
+const ASSIGN_TESTS = {
+  'Method `__assign` with specified type': {
+    true: () => testing
+      .before(component => {
+        component.__create(DynamicComponent);
+        component.model = { defined: false };
+        component.__assign<{ defined: boolean }, ComponentLoaderClassComponent>('model', component);
+      })
+      .equal('__component.instance.model', { defined: false })
+  },
+  'Method `__assign` with specified type and list of properties': {
+    true: () =>
+      testing
+        .before(component => component
+          .__create(DynamicComponent)
+          .__assign<string, ComponentLoaderClassComponent>(['__prefix', '__suffix'], component)
+        )
+        .be<string>([
+          '__prefix',
+          '__component.instance.__prefix',
+          '__suffix',
+          '__component.instance.__suffix'
+        ], '_')
+  },
+  '`__component` instance model and key with array argument should be changed': {
+    true: () => testing
+      .before(component => {
+        component.__create(DynamicComponent);
+        component.model = { defined: false };
+        component.__assign(['key', 'model'], component);
+      })
+      .be<string>('__component.instance.key', 'defined')
+      .equal<Object>('__component.instance.model', { defined: false })
+  }
+};
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [TestModule]
-    })
-      .compileComponents();
-  }));
+const CONNECT_TESTS = {
+  'Property `key` and `model should be connected with dynamic instance component': {
+    true: () => {
+      // Prepare values to assign.
+      const key = 'connected';
+      const model = { defined: false };
 
-  // synchronous beforeEach
-  beforeEach(() => {
-    // fixture = TestBed.createComponent(ComponentLoaderClassComponent);
-    testingClass.fixture = TestBed.createComponent(ComponentLoaderClassComponent);
-  });
-
-  /**
-   * Main tests.
-   */
-  const MAIN_TESTS = {
-    '<div>': {
-      true: () => testingClass.selector('div')
-    },
-    '`__component` property undefined': {
-      true: () => testingClass.undefined('__component')
-    },
-    '`__component` property defined': {
-      true: () => testingClass
-        .before(component => component.__create(DynamicComponent))
-        .defined('__component')
-    },
-    'Dynamic component model should be defined': {
-      true: () => testingClass
-        .before(component => component.__create(DynamicComponent))
-        .defined('__component.instance.model')
-    }
-  };
-
-  /**
-   * Method `__assign()`.
-   */
-  const ASSIGN_TESTS = {
-    'Method `__assign` with specified type': {
-      true: () => testingClass
+      testing
         .before(component => {
-          component.__create(DynamicComponent);
-          component.model = { defined: false };
-          component.__assign<{ defined: boolean }, ComponentLoaderClassComponent>('model', component);
+          component
+            // Create component.
+            .__create(DynamicComponent);
         })
-        .equal('__component.instance.model', { defined: false })
-    },
-    'Method `__assign` with specified type and list of properties': {
-      true: () =>
-        testingClass
-          .before(component => component
-            .__create(DynamicComponent)
-            .__assign<string, ComponentLoaderClassComponent>(['__prefix', '__suffix'], component)
-          )
-          .be<string>([
-            '__prefix',
-            '__component.instance.__prefix',
-            '__suffix',
-            '__component.instance.__suffix'
-          ], '_')
-    },
-    '`__component` instance model and key with array argument should be changed': {
-      true: () => testingClass
+        // Check properties after creation.
+        .be({
+          '__component.instance.key': 'notdefined',
+          key: 'defined'
+        })
         .before(component => {
-          component.__create(DynamicComponent);
-          component.model = { defined: false };
-          component.__assign(['key', 'model'], component);
-        })
-        .be<string>('__component.instance.key', 'defined')
-        .equal<Object>('__component.instance.model', { defined: false })
-    }
-  };
-
-  const CONNECT_TESTS = {
-    'Property `key` and `model should be connected with dynamic instance component': {
-      true: () => {
-        // Prepare values to assign.
-        const key = 'connected';
-        const model = { defined: false };
-
-        testingClass
-          .before(component => {
-            component
-              // Create component.
-              .__create(DynamicComponent);
-          })
-          // Check properties after creation.
-          .be({
-            '__component.instance.key': 'notdefined',
-            key: 'defined'
-          })
-          .before(component => {
-            // Link indicated properties the same component to target component.
-            component.__bind(['key', 'model']);
-            // Assign prepared values to component.
-            Object.assign(component, {
-              key, model
-            });
-          })
-          .be({
-            '__component.instance.key': key,
-            '__component.instance.model': model
+          // Link indicated properties the same component to target component.
+          component.__bind(['key', 'model']);
+          // Assign prepared values to component.
+          Object.assign(component, {
+            key, model
           });
-      }
-    },
-    'Property `wrapped` set and get from component should be working': {
-      true: () => {
-        testingClass
-          .before(component => {
-            component
-              // Create component.
-              .__create(DynamicComponent)
-              .__link(['wrapped']);
-            component.wrapped = true;
-          })
-          .truthy([
-            '_wrapped_',
-            '_wrappedGet',
-            '_wrappedSet',
-            'wrapped',
-            '_wrapped',
-            '__component.instance.wrapped'
-          ]);
-
-      }
+          console.info(component.__component.instance);
+        })
+        .be({
+          '__component.instance.key': key,
+          '__component.instance.model': model
+        });
     }
-  };
-
-  const DESTROY_TESTS = {
-    '`__component` should be destroyed': {
-      true: () => testingClass
-        .before(component => component
-          // .__create(DynamicComponent)
-          .__destroy())
-        .undefined('__component')
-    }
-  };
-
-  /**
-   * Method `__get()`.
-   */
-  const GET_TESTS = {
-    'Method `__get` with specified type `boolean` working properly': {
-      true: () => testingClass
-        .before(component => component
-          .__create(DynamicComponent)
-          .__get<boolean>('wrapped'))
-        .falsy()
-    },
-    'Method `__get` with specified type `number` working properly': {
-      true: () => testingClass
-        .before(component => component
-          .__create(DynamicComponent)
-          .__get<number>('age'))
-        .equal(27)
-    },
-    'Method `__get` with specified type `string` working properly': {
-      true: () => testingClass
-        .before(component => component
-          .__create(DynamicComponent)
-          .__get<string>('key'))
-        .equal<string>('notdefined')
-    },
-    'Method `__get` with specified type `object` working properly': {
-      true: () => testingClass
-        .before(component => component
-          .__create(DynamicComponent)
-          .__get<{ defined: boolean }>('model'))
-        .equal({ defined: true })
-    }
-  };
-
-  /**
-   * Method `__set()`.
-   */
-  const SET_TESTS = {
-    'Method `__set`': {
-      true: () => {
-        const value = 'assigned';
-        testingClass
-          .before(component => component
+  },
+  'Property `wrapped` set and get from component should be working': {
+    true: () => {
+      testing
+        .before(component => {
+          component
+            // Create component.
             .__create(DynamicComponent)
-            .__set('assign', value))
-          .equal('__component.instance.assign', value);
-      }
-    },
-    '`__set` with specified type `string` working properly': {
-      true: () => {
-        const value = 'assigned';
-        testingClass
-          .before(component => component
-            .__create(DynamicComponent)
-            .__set<string>('assign', value))
-          .equal('__component.instance.assign', value);
-      }
-    },
-    'Method `__set` with specified type `boolean` working properly': {
-      true: () => {
-        testingClass
-          .before(component => component
-            .__create(DynamicComponent)
-            .__set<boolean>('wrapped', true))
-          .truthy('__component.instance.wrapped');
-      }
-    },
-    'Method `__set` with specified type `object` working properly': {
-      true: () => {
-        const value = { defined: false };
-        testingClass
-          .before(component => component
-            .__create(DynamicComponent)
-            .__set<{ defined: boolean }>('model', value))
-          .equal<{ defined: boolean }>('__component.instance.model', value);
-      }
-    }
-  };
+            .__link(['wrapped']);
+          component.wrapped = true;
+        })
+        .truthy([
+          '_wrapped_',
+          '_wrappedGet',
+          '_wrappedSet',
+          'wrapped',
+          '_wrapped',
+          '__component.instance.wrapped'
+        ]);
 
-  /**
-   * Method `__subscribe()`.
-   */
-  const SUBSCRIBE_TESTS = {
-    '`__component` instance subscribe to event EventEmitter': {
-      true: () => testingClass
-        .before(component => component
-          .__create(DynamicComponent)
-          .__subscribe('event', (generatorOrNext: any) => {
-            expect(generatorOrNext)
-              .toBe('event');
-          }, (error: any) => error, (complete: any) => complete))
-        .property<DynamicComponent>('__component.instance')
-        .emit()
-    },
-    '`__component` instance subscribe to event EventEmitter and emit complete': {
-      true: () => testingClass
-        .before(component => component
-          .__create(DynamicComponent)
-          .__subscribe('event',
-          (result: any) => result,
-          (error: any) => error,
-          (complete: any) => complete))
-        .property<DynamicComponent>('__component.instance')
-        .emitComplete()
     }
-  };
+  }
+};
+
+const DESTROY_TESTS = {
+  '`__component` should be destroyed': {
+    true: () => testing
+      .before(component => component
+        // .__create(DynamicComponent)
+        .__destroy())
+      .undefined('__component')
+  }
+};
+
+/**
+ * Method `__get()`.
+ */
+const GET_TESTS = {
+  'Method `__get` with specified type `boolean` working properly': {
+    true: () => testing
+      .before(component => component
+        .__create(DynamicComponent)
+        .__get<boolean>('wrapped'))
+      .falsy()
+  },
+  'Method `__get` with specified type `number` working properly': {
+    true: () => testing
+      .before(component => component
+        .__create(DynamicComponent)
+        .__get<number>('age'))
+      .equal(27)
+  },
+  'Method `__get` with specified type `string` working properly': {
+    true: () => testing
+      .before(component => component
+        .__create(DynamicComponent)
+        .__get<string>('key'))
+      .equal<string>('notdefined')
+  },
+  'Method `__get` with specified type `object` working properly': {
+    true: () => testing
+      .before(component => component
+        .__create(DynamicComponent)
+        .__get<{ defined: boolean }>('model'))
+      .equal({ defined: true })
+  }
+};
+
+/**
+ * Method `__set()`.
+ */
+const SET_TESTS = {
+  'Method `__set`': {
+    true: () => {
+      const value = 'assigned';
+      testing
+        .before(component => component
+          .__create(DynamicComponent)
+          .__set('assign', value))
+        .equal('__component.instance.assign', value);
+    }
+  },
+  '`__set` with specified type `string` working properly': {
+    true: () => {
+      const value = 'assigned';
+      testing
+        .before(component => component
+          .__create(DynamicComponent)
+          .__set<string>('assign', value))
+        .equal('__component.instance.assign', value);
+    }
+  },
+  'Method `__set` with specified type `boolean` working properly': {
+    true: () => {
+      testing
+        .before(component => component
+          .__create(DynamicComponent)
+          .__set<boolean>('wrapped', true))
+        .truthy('__component.instance.wrapped');
+    }
+  },
+  'Method `__set` with specified type `object` working properly': {
+    true: () => {
+      const value = { defined: false };
+      testing
+        .before(component => component
+          .__create(DynamicComponent)
+          .__set<{ defined: boolean }>('model', value))
+        .equal<{ defined: boolean }>('__component.instance.model', value);
+    }
+  }
+};
+
+/**
+ * Method `__subscribe()`.
+ */
+const SUBSCRIBE_TESTS = {
+  '`__component` instance subscribe to event EventEmitter': {
+    true: () => testing
+      .before(component => component
+        .__create(DynamicComponent)
+        .__subscribe('event', (generatorOrNext: any) => {
+          expect(generatorOrNext)
+            .toBe('event');
+        }, (error: any) => error, (complete: any) => complete))
+      .property<DynamicComponent>('__component.instance')
+      .emit()
+  },
+  '`__component` instance subscribe to event EventEmitter and emit complete': {
+    true: () => testing
+      .before(component => component
+        .__create(DynamicComponent)
+        .__subscribe('event',
+        (result: any) => result,
+        (error: any) => error,
+        (complete: any) => complete))
+      .property<DynamicComponent>('__component.instance')
+      .emitComplete()
+  }
+};
 
   /*
     __componentPropertyName
@@ -312,15 +280,15 @@ describe('ComponentLoaderClass', () => {
   }));
   */    
 
-  testingClass.eachIt(
-    ASSIGN_TESTS,
-    CONNECT_TESTS,
-    DESTROY_TESTS,
-    MAIN_TESTS,
-    GET_TESTS,
-    SET_TESTS,
-    SUBSCRIBE_TESTS
-  );
+testing.execute(
+  ASSIGN_TESTS,
+  CONNECT_TESTS,
+  DESTROY_TESTS,
+  MAIN_TESTS,
+  GET_TESTS,
+  SET_TESTS,
+  SUBSCRIBE_TESTS
+);
 
   /*
     __componentPropertyName
@@ -350,4 +318,3 @@ describe('ComponentLoaderClass', () => {
       .toBe('defined');
   }));
   */
-});
