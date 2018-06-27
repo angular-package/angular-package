0645455fdd3bfaa96691b204d9fe1c87178e39f2
angularpackage <angular-package@wwwdev.io>
2018-06-27 13:43:01 +0200

    refactor(ArgumentHandlerClass): Change style, add jsdoc, move type outside.
    - Move type Data<T> (Argument<T>) to type directory.

--- packages/core/packages/handler/src/argument.handler.class.ts
+++ packages/core/packages/handler/src/argument.handler.class.ts
@@ -1,26 +1,33 @@
 
-export type Data<T> = Array<any> | boolean | number | string | { [property: string]: T };
+import { Argument } from '../../type';
 
 export class ArgumentHandlerClass {
 
+  /**
+   * Spread argument values to object.
+   * @param args Array object.
+   */
   spreadToObject(...args: Array<Object>): Object {
     let obj = {};
-    args.forEach((add: Object) => {
-      obj = { ...obj, ...add };
-    });
+    args.forEach((add: Object) => obj = { ...obj, ...add });
 
     return obj;
   }
 
-  toArray<T>(result: Data<T>): Array<string> {
+  /**
+   * Argument value to array.
+   * @template T Argument type.
+   */
+  toArray<T>(result: Argument<T>): Array<T> {
     if (Array.isArray(result)) {
       return result;
     }
     if (result instanceof Object) {
-      return Object.keys(result);
+      const r: Array<any> = Object.keys(result);
+
+      return r;
     }
 
     return [result];
   }
-
 }
