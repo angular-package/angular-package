9b4c455d2f769173c44cb27a5dca3d9951a91b63
angularpackage <angular-package@wwwdev.io>
2018-06-29 16:15:44 +0200

    refactor(interface): Add export `ConsoleLog`.

--- packages/core/packages/testing/interface/index.ts
+++ packages/core/packages/testing/interface/index.ts
@@ -1,4 +1,4 @@
-
+export { ConsoleLog } from './console-log.interface';
 export { Options } from './options.interface';
 export { Spec } from './spec.interface';
 export { TestingOptions } from './testing-options.interface';
