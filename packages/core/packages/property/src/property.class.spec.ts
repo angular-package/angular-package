788be88ccdfed5eb2f648a3b9ece25908cb4a254
angularpackage <angular-package@wwwdev.io>
2018-06-29 14:55:05 +0200

    test(PropertyClass): Change the way of testing class by using `TestingClass`.

--- packages/core/packages/property/src/property.class.spec.ts
+++ packages/core/packages/property/src/property.class.spec.ts
@@ -1,108 +1,90 @@
-// Make describe visible.
-import {
-
-} from 'jasmine';
-
-// external
-// import { NO_ERRORS_SCHEMA, ViewChild, ElementRef, DebugElement } from '@angular/core';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-// import { By } from '@angular/platform-browser';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 // internal
 import { TestPropertyComponent } from '../test/component';
 import { PropertyClass } from '.';
+import { TestingClass } from '../../testing';
+import { Options } from '../../testing/interface';
 
-beforeAll(() => {
-  TestBed.resetTestEnvironment();
-  TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-});
+const TESTING_OPTIONS: Options = {
+  console: {
+    executed: true,
+    notExecuted: true
+  },
+  execute: []
+};
 
-describe('TestPropertyComponent', () => {
+const testing: TestingClass<TestPropertyComponent> =
+  new TestingClass<TestPropertyComponent>('TestPropertyComponent', {
+    declarations: [
+      TestPropertyComponent
+    ]
+  },
+    TestPropertyComponent,
+    TESTING_OPTIONS);
 
-  let comp: TestPropertyComponent;
-  let fixture: ComponentFixture<TestPropertyComponent>;
-  // let debugElement: DebugElement;
-  // let nativeElement: ElementRef;
+testing
+  .spec({
+    'should have `propertyClass` instance.': () => testing
+      .before(comp => comp.propertyClass instanceof PropertyClass)
+      .truthy(),
+    'should have bind working when properties is string.': () => testing
+      .before(comp => {
+        comp.propertyClass.bind(comp, 'firstname', 'target');
+        comp.firstname = 'Lucas';
+      })
+      .equal('target.firstname', 'Lucas'),
+    'should have bind working when properties is array of string.': () => {
+      testing
+        .before(comp => {
+          comp.propertyClass.bind(comp, ['firstname', 'surname'], 'target');
+          comp.firstname = 'Lucas';
+          comp.surname = 'Tramp';
+        })
+        .equal(['firstname', 'target.firstname'], 'Lucas')
+        .equal(['surname', 'target.surname'], 'Tramp')
+    },
+    'should have firstname changed after bind with array of string.': () => {
+      const firstname = 'Lucas';
+      const surname = 'Tramp';
+      testing
+        .before(comp => {
+          comp.propertyClass.bind(comp, ['firstname', 'surname'], 'target');
+          comp.firstname = firstname;
+          comp.surname = surname;
+        })
+        .equal(['firstname', 'target.firstname'], firstname)
+        .equal(['surname', 'target.surname'], surname)
+        .set<string>('firstname', 'Donald')
+        .equal(['firstname', 'target.firstname'], 'Donald');
+    }
+  })
+  .execute([], );
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        TestPropertyComponent
-      ]
-    })
-      .compileComponents();
-  }));
-
-  // synchronous beforeEach
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestPropertyComponent);
-    // debugElement = fixture.debugElement;
-    // nativeElement = fixture.nativeElement;
-    comp = fixture.componentInstance;
+testing
+  .spec({
+    'Remove binded.': () => testing
+      .before(comp => {
+        comp.propertyClass.bind(comp, ['firstname', 'surname'], 'target');
+        if (comp.propertyClass.binded instanceof Array) {
+          const index: number = comp.propertyClass.binded.indexOf('surname');
+          if (index > -1) {
+            comp.propertyClass.binded = index;
+          }
+        }
+        if (comp.propertyClass.binded instanceof Array) {
+          return comp.propertyClass.binded.indexOf('surname');
+        }
+      })
+      .equal(-1),
+    '': () => {}
   });
 
-  it('#1. should create test component', async(() => {
-    expect(fixture)
-      .toBeDefined();
-    expect(comp)
-      .toBeTruthy();
-  }));
+/*
 
-  it('#2. should have `propertyClass` instance.', async(() => {
-    expect(comp.propertyClass instanceof PropertyClass)
-      .toBeTruthy();
-  }));
-
-  /*
-    `bind()`
-  */
-  it('#3. should have bind working when properties is string.', async(() => {
-    comp.propertyClass.bind(comp, 'firstname', 'target');
-    comp.firstname = 'Lucas';
-    expect(comp.firstname)
-      .toEqual(comp.target.firstname);
-  }));
-
-  it('#4. should have bind working when properties is array of string.', async(() => {
-    comp.propertyClass.bind(comp, ['firstname', 'surname'], 'target');
-    comp.firstname = 'Lucas';
-    comp.surname = 'Tramp';
-    expect(comp.firstname)
-      .toEqual(comp.target.firstname);
-    expect(comp.surname)
-      .toEqual(comp.target.surname);
-  }));
-
-  it('#5. should have change firstname when binded with array of string.', async(() => {
-    comp.propertyClass.bind(comp, ['firstname', 'surname'], 'target');
-    comp.firstname = 'Lucas';
-    comp.surname = 'Tramp';
-    expect(comp.firstname)
-      .toEqual(comp.target.firstname);
-    expect(comp.surname)
-      .toEqual(comp.target.surname);
-    comp.firstname = 'Donald';
-    expect(comp.firstname)
-      .toEqual(comp.target.firstname);
-  }));
+/*
 
   /*
     `binded`
-  */
-  it('#6. Remove binded.', async(() => {
-    comp.propertyClass.bind(comp, ['firstname', 'surname'], 'target');
-    if (comp.propertyClass.binded instanceof Array) {
-      const index: number = comp.propertyClass.binded.indexOf('surname');
-      if (index > -1) {
-        comp.propertyClass.binded = index;
-      }
-    }
-    if (comp.propertyClass.binded instanceof Array) {
-      expect(comp.propertyClass.binded.indexOf('surname'))
-        .toEqual(-1);
-    }
-  }));
 
   it('#7. Do not remove binded when index is string.', async(() => {
     comp.propertyClass.bind(comp, ['firstname', 'surname'], 'target');
@@ -128,3 +110,4 @@ describe('TestPropertyComponent', () => {
     comp.propertyClass.clear(comp, 'firstname');
   }));
 });
+*/
