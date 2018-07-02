97b75398f33bc1a407fcaf224524e980f489c6d3
angularpackage <angular-package@wwwdev.io>
2018-07-02 16:13:30 +0200

    refactor(ConsoleLog): Add description about possibilities.

--- packages/core/packages/testing/type/console-log.type.ts
+++ packages/core/packages/testing/type/console-log.type.ts
@@ -1,1 +1,8 @@
+/**
+ * Four Options are available:
+ * 1. boolean `true` = Both executed and notExecuted specs are logged.
+ * 2. boolean `false` = Executed and notExecuted specs are'nt logged.
+ * 3. string `executed` = Executed specs are logged.
+ * 4. string `notExecuted` = notExecuted specs are logged.
+ */ 
 export type ConsoleLog = boolean | 'executed' | 'notExecuted';
