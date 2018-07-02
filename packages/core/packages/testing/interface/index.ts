efbbd3ad0547388cbabb021b60e433aefe081932
angularpackage <angular-package@wwwdev.io>
2018-07-02 16:12:14 +0200

    refactor(interface): remove ConsoleLog.

--- packages/core/packages/testing/interface/index.ts
+++ packages/core/packages/testing/interface/index.ts
@@ -1,4 +1,3 @@
-export { ConsoleLog } from './console-log.interface';
 export { Options } from './options.interface';
 export { Spec } from './spec.interface';
 export { TestingOptions } from './testing-options.interface';
