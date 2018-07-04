541e244f60c9bae0ae09537b94c176d320bfeac2
angularpackage <angular-package@wwwdev.io>
2018-07-04 14:51:33 +0200

    fix(ConsoleLog): Add 'r' to export file name.

--- packages/core/packages/testing/interface/testing-options.interface.ts
+++ packages/core/packages/testing/interface/testing-options.interface.ts
@@ -1,4 +1,4 @@
-import { ConsoleLog } from './console-log.interface';
+import { ConsoleLog } from './console-log.inteface';
 import { Execute } from '../type';
 export interface TestingOptions {
   console?: ConsoleLog;
