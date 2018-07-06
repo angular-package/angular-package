d4edb1b4da61a2db75820d701d15a6dd43f397c0
angularpackage <angular-package@wwwdev.io>
2018-07-06 16:45:15 +0200

    refactor(ConsoleClass): Change properties and methods, add and remove methods, use new types.
    Add:
    - property `styles` to store console style.
    
    Changed:
    - property `colors` to have only colors.
    - methods `blue()`, `green()`, `red(), `yellow()` to have arguments `style` type of `ConsoleStyle`.
    - method `text()` name to `prepare()` name. Now method `text()` is used to prepare text with specific color and style.
    
    Removed:
    - method `faint()` because it is style.

--- packages/core/packages/src/console.class.ts
+++ packages/core/packages/src/console.class.ts
@@ -1,49 +1,47 @@
+import { ConsoleColor, ConsoleStyle } from '../type';
 
 export class ConsoleClass {
   private colors = {
-    use: '',
-    defined: {
-      reset: '\x1b[0m',
-      faint: '\x1b[2m',
-  
-      // text color
-      black: '\x1b[30m',
-      red: '\x1b[31m',
-      green: '\x1b[32m',
-      yellow: '\x1b[33m',
-      blue: '\x1b[34m',
-      magenta: '\x1b[35m',
-      cyan: '\x1b[36m',
-      white: '\x1b[37m',
-  
-      // background color
-      defaulgBG: '\x1b[39m',
-      blackBg: '\x1b[40m',
-      redBg: '\x1b[41m',
-      greenBg: '\x1b[42m',
-      yellowBg: '\x1b[43m',
-      blueBg: '\x1b[44m',
-      magentaBg: '\x1b[45m',
-      cyanBg: '\x1b[46m',
-      whiteBg: '\x1b[47m'
-    }
+    default: '\x1b[0m',
+    reset: '\x1b[0m',
+
+    // text color
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+
+    // background color
+    defaulgBG: '\x1b[39m',
+    blackBg: '\x1b[40m',
+    redBg: '\x1b[41m',
+    greenBg: '\x1b[42m',
+    yellowBg: '\x1b[43m',
+    blueBg: '\x1b[44m',
+    magentaBg: '\x1b[45m',
+    cyanBg: '\x1b[46m',
+    whiteBg: '\x1b[47m'
+  };
+
+  private styles = {
+    bold: '\x1b[1m',
+    reset: '\x1b[0m',
+    faint: '\x1b[2m'
   };
   private _text = '';
 
-  blue(text: string): this {
-    this.text(text, 'blue');
+  blue(text: string, style?: ConsoleStyle): this {
+    this.prepare(text, 'blue', style);
 
     return this;
   }
 
-  faint(text: string): this {
-    this.text(text, 'faint');
-
-    return this;
-  }
-
-  green(text: string): this {
-    this.text(text, 'green');
+  green(text: string, style?: ConsoleStyle): this {
+    this.prepare(text, 'green', style);
 
     return this;
   }
@@ -55,26 +53,41 @@ export class ConsoleClass {
     return this;
   }
 
-  log(): this {
-    console.log(`${this.colors.defined.reset} ${ this._text } ${this.colors.defined.reset}`);
-    this._text = '';
+  log(display: boolean = true): this {
+    if (display === true) {
+      console.log(`${this.styles.reset} ${ this._text } ${this.styles.reset}`);
+      this._text = '';
+    }
 
     return this;
   }
 
-  red(text: string): this {
-    this.text(text, 'red');
+  red(text: string, style?: ConsoleStyle): this {
+    this.prepare(text, 'red', style);
 
     return this;
   }
 
-  yellow(text: string): this {
-    this.text(text, 'yellow');
+  text(text: string, color?: ConsoleColor, style?: ConsoleStyle): this {
+    this.prepare(text, color, style);
 
     return this;
   }
 
-  private text(text: string, color: string = ''): string {
-    return this._text = `${this._text}${this.colors.defined[color]}${text}${this.colors.defined.reset}`;
+  yellow(text: string, style?: ConsoleStyle): this {
+    this.prepare(text, 'yellow', style);
+
+    return this;
+  }
+
+  private prepare(text: string, color: ConsoleColor = 'reset', style?: ConsoleStyle): string {
+    let styles = '';
+    if (style) {
+      style.forEach((s: string) => {
+        styles = `${styles}${this.styles[s]}`;
+      });  
+    }
+
+    return this._text = `${this._text}${this.colors[color]}${styles}${text}${this.styles.reset}`;
   }
 }
