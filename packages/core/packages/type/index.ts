fe69678dcb52ecefea8fa893cc7cd3e78fe098f8
angularpackage <angular-package@wwwdev.io>
2018-07-06 16:39:50 +0200

    feat(type): Add `ConsoleStyle` and `ConsoleColor` types for `ConsoleClass`.

--- packages/core/packages/type/index.ts
+++ packages/core/packages/type/index.ts
@@ -1,4 +1,6 @@
 export { Argument } from './argument.type';
+export { ConsoleColor } from './console-color.type';
+export { ConsoleStyle } from './console-style.type';
 export { CycleHook } from './cycle-hook.type';
 export { GenericObject } from './generic-object.type';
 export { Matcher } from './matcher.type';
