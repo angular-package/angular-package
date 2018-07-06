0fcb10dd60d2476e16479e9acdc862ae4f2fcca6
angularpackage <angular-package@wwwdev.io>
2018-07-06 14:44:35 +0200

    refactor: Export `ConsoleClass` feature.

--- packages/core/packages/src/index.ts
+++ packages/core/packages/src/index.ts
@@ -1,3 +1,4 @@
+export { ConsoleClass } from './console.class';
 export { getProperty } from './get-property.func';
 export { PrefixSuffixClass } from './prefixsuffix.class';
 export { setProperty } from './set-property.func';
