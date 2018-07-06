6ed679045859587659a35ab0539645d85d632988
angularpackage <angular-package@wwwdev.io>
2018-07-06 16:59:38 +0200

    refactor(interface): Add exports `SpecOptions`, change `ConsoleLog` to `Log`.

--- packages/core/packages/testing/interface/index.ts
+++ packages/core/packages/testing/interface/index.ts
@@ -1,4 +1,5 @@
-export { ConsoleLog } from './console-log.inteface';
+export { Log } from './log.interface';
 export { Options } from './options.interface';
 export { Spec } from './spec.interface';
+export { SpecOptions } from './spec-options.interface';
 export { TestingOptions } from './testing-options.interface';
