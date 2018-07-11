63707c9145795d42019cfbc87303f1a608b0ee75
angularpackage <angular-package@wwwdev.io>
2018-07-11 20:00:02 +0200

    refactor(interface): Change file and exported names.
    No more export:
    - `SpecOptions` from `spec-options.interface.ts`
    - `TestingOptions` from `testing-options.interface`.
    
    Added:
    - Export `SpecSettings`, `Settings`.

--- packages/core/packages/testing/interface/index.ts
+++ packages/core/packages/testing/interface/index.ts
@@ -1,5 +1,5 @@
 export { Log } from './log.interface';
 export { Options } from './options.interface';
 export { Spec } from './spec.interface';
-export { SpecOptions } from './spec-options.interface';
-export { TestingOptions } from './testing-options.interface';
+export { SpecSettings } from './spec-settings.interface';
+export { Settings } from './settings.interface';
