920a4ca133d585844b696c748bb62b43f3aec870
angularpackage <angular-package@wwwdev.io>
2018-07-12 16:41:25 +0200

    refactor(ConsoleLog): Change value 'notExecuted' to jasmine 'skipped'.

--- packages/core/packages/testing/type/console-log.type.ts
+++ packages/core/packages/testing/type/console-log.type.ts
@@ -1,8 +1,8 @@
 /**
  * Four Options are available:
- * 1. boolean `true` = Both executed and notExecuted specs are logged.
- * 2. boolean `false` = Executed and notExecuted specs are'nt logged.
+ * 1. boolean `true` = Both executed and skipped specs are logged.
+ * 2. boolean `false` = Executed and skipped specs are'nt logged.
  * 3. string `executed` = Executed specs are logged.
- * 4. string `notExecuted` = notExecuted specs are logged.
+ * 4. string `skipped` = skipped specs are logged.
  */ 
-export type ConsoleLog = boolean | 'executed' | 'notExecuted';
+export type ConsoleLog = boolean | 'executed' | 'skipped';
