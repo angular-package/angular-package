c083a4c295e17c246e61e13a0b638c3e42f9f238
angularpackage <angular-package@wwwdev.io>
2018-07-13 16:29:31 +0200

    refactor(SpecIt): Add jsdoc.

--- packages/core/packages/testing/type/spec-it.type.ts
+++ packages/core/packages/testing/type/spec-it.type.ts
@@ -1,3 +1,6 @@
 import { Spec, SpecSettings } from '../interface';
 
+/**
+ * Type of callback function executed when using `eachIt()` method.
+ */
 export type SpecIt = (specsName: Array<string>, specs: Spec, settings: SpecSettings, execution: (number: number) => boolean) => any;
