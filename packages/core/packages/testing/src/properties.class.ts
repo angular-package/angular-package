439fb459f3a50a576daf826d7a901c9495c3e22a
angularpackage <angular-package@wwwdev.io>
2018-07-13 16:21:48 +0200

    refactor(PropertiesClass): Update properties, change method names, update descriptions.
    Added:
    - js doc description to all properties.
    - property `specDescription` to store `spec()` method description.
    
    Changed:
    - property `options` to `settings` because of its real purpose.
    - property `originalOptions` to `storedSettings` to have more intuitive names.
    - method `restoreOriginalOptions()` to `restoreSettings()`.
    - method `setOptions()` to `setSettings()` and use `skipped` instead of `notExecuted`.

--- packages/core/packages/testing/src/properties.class.ts
+++ packages/core/packages/testing/src/properties.class.ts
@@ -6,38 +6,75 @@ import { ComponentFixture, TestModuleMetadata } from '@angular/core/testing';
 
 // internal
 import { ArgumentHandlerClass } from '../../handler';
-import { Options, Spec, TestingOptions } from '../interface';
+import { Options, Settings, Spec } from '../interface';
 import { ResultName } from '../type';
 import { PropertyClass } from '../../property';
 import { ConsoleClass } from '../../src';
 
 export abstract class PropertiesClass<T> extends ArgumentHandlerClass {
-
+  /**
+   * Angular `componentInstance` fixture.
+   */
   componentInstance: T;
+
+  /**
+   * Angular `debugElement` fixture.
+   */
   debugElement: DebugElement;
 
+  /**
+   * Angular `nativeElement` fixture.
+   */
   nativeElement: HTMLElement | ElementRef;
 
+  /**
+   * Object to display logs with specific colors.
+   */
   protected consoleClass = new ConsoleClass();
-  protected originalOptions: TestingOptions;
-  protected options?: TestingOptions = {
-    console: {
-      executed: false,
-      notExecuted: false
-    },
-    execute: []
-  };
 
+  /**
+   * Object to handle property features.
+   */
   protected propertyClass: PropertyClass = new PropertyClass();
 
+  /**
+   * Stored result of `before()` `query` method with last name.
+   */
   protected result: {
     before?: any,
     query?: DebugElement,
     name: ResultName
   } = { name: undefined };
 
+  /**
+   * Settings that the chosen options are in.
+   */
+  protected settings?: Settings = {
+    console: {
+      executed: false,
+      skipped: false
+    },
+    execute: []
+  };
+
+  /**
+   * Second description that is being added after primary `description` and it's set when invoking `spec()` method.
+   */
+  protected specDescription = '';
+
+  /**
+   * List of specs for execution.
+   */
   protected specs: Spec = {};
-  
+
+  /**
+   * Stored settings, used in `spec()` method.
+   */
+  protected storedSettings: Settings;
+
+  /**
+   * Get and set all needed element from fixture to specific properties in object.
+   */
   private _fixture: ComponentFixture<T>;
   set fixture(fixture: ComponentFixture<T>) {
     this._fixture = fixture;
@@ -53,7 +90,7 @@ export abstract class PropertiesClass<T> extends ArgumentHandlerClass {
    * @param description Jasmine textual description of the main group.
    * @param moduleDef Angular module definition.
    * @param componentTest Component to test.
-   * @param [options] Console information and execute control.
+   * @param [options] Used to set console information and execute control.
    */
   constructor(
     protected description: string,
@@ -62,32 +99,39 @@ export abstract class PropertiesClass<T> extends ArgumentHandlerClass {
     options?: Options
   ) {
     super();
-    this.setOptions(options);
-    this.originalOptions = { ...{}, ...this.options };
+    this.setSettings(options);
+    this.storedSettings = { ...{}, ...this.settings };
 
     return this;
   }
 
-  protected restoreOriginalOptions(): this {
-    this.options = { ...{}, ...this.options, ...this.originalOptions };
+  /**
+   * Restores settings to default, or to settings that was set on instantiation.
+   */
+  protected restoreSettings(): this {
+    this.settings = { ...{}, ...this.settings, ...this.storedSettings };
 
     return this;
   }
 
-  protected setOptions(options: Options): this {
+  /**
+   * Set settings with specific options.
+   * @param options Argument executed or log to set settings.
+   */
+  protected setSettings(options: Options): this {
     // Set console.
     if (typeof options.log === 'string') {
-      this.options.console = { executed: false, notExecuted: false };
-      this.options.console[options.log] = true ;
+      this.settings.console = { executed: false, skipped: false };
+      this.settings.console[options.log] = true ;
     } else if (typeof options.log === 'boolean') {
-      this.options.console = {
+      this.settings.console = {
         executed: options.log,
-        notExecuted: options.log
+        skipped: options.log
       };
     }
     // Set execute `false` or `Array<number>`.
     if (options.execute !== undefined) {
-      this.options.execute = options.execute;
+      this.settings.execute = options.execute;
     }
 
     return this;
