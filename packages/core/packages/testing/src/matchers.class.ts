ea7ba9fbb0aeb15739c6bc1fa0f581ac5db76c1b
angularpackage <angular-package@wwwdev.io>
2018-07-13 16:28:49 +0200

    refactor(MatchersClass): Add change methods, add jsdoc, change arguments names, add properties.
    Added:
    - `_not` property defaultly `false` and getter `not()` for `_not`.
    - Some method argument `expectationFailOutput` as in jasmine.
    - `switch()` and `matcher()` method  to support arguments auto-detection.
    
    Changed:
    - arguments `properties` some of methods from type `Data<T>` to `Argument<T>` and name `actualOrExpected`.
    
    Removed:
    - `test()` method because it is in `SelectorClass`.

--- packages/core/packages/testing/src/matchers.class.ts
+++ packages/core/packages/testing/src/matchers.class.ts
@@ -1,57 +1,316 @@
 import {
-
+  
 } from 'jasmine';
-import { By } from '@angular/platform-browser';
-import { get } from 'lodash-es';
 
+// internal
 import { MainClass } from './main.class';
-import { Data } from '../../handler/src/argument.handler.class';
+import { typeGuard } from '../../src';
+import { Argument, Matcher } from '../../type';
 
 /**
  * @export
  */
-export class MatchersClass<T> extends MainClass<T> {
+export abstract class MatchersClass<T> extends MainClass<T> {
 
-  be<V>(properties: Data<V>, value?: V): this {
-    this.toArray(properties)
-      .forEach(property => {
-        if (value) {
-          this._be<V>(property, value);
-        } else {
-          this._be<V>(property, properties[property]);
-        }
-      });
+  _not = false;
+  get not(): this {
+    this._not = true;
 
     return this;
   }
 
   /**
-   * @param contain String to be contained in innerHTML.
+   * Actual value to be expected value.
+   * @template V Expected type.
+   * @param actualOrExpected Acts as expected value when chaining, actual value as component property name,
+   * actual value as component property name list or as component property name with defined value to test expectations against.
+   * @param [expected] "The actual value to be equal to the expected, using deep equality comparison".
+   * @param [expectationFailOutput] Fail output.
+   * @tested #1-11
    */
-  contain(contain: string): this {
-    expect(this.debugElement.nativeElement.innerHTML)
-      .toContain(contain);
+  be<V>(actualOrExpected: Argument<V>, expected?: V, expectationFailOutput?: any): this {
+    this.matcher<V>('be', actualOrExpected, expected, expectationFailOutput);
 
     return this;
   }
 
-  defined(property: string): this {
-    expect(get(this.comp, property))
-      .toBeDefined();
+  /*
+  beSpread<V>(...args: Array<Argument<V>>): this {
+    const expected: any = args[args.length - 1];
+    args.map((actualOrExpected, index) => {
+        if (args.length > 1) {
+          if (index !== args.length - 1) {
+            this._be<V>(actualOrExpected, expected);
+          }  
+        } else if(typeof actualOrExpected === 'string') {
+          this._be<V>(undefined, actualOrExpected);
+        }
+      });
+
+    return this;
+  }
+  */
+
+  /**
+   * Expect the actual value to contain a specific value.
+   * @template V Expected type.
+   * @param actualOrContain Acts as expected value when chaining, actual value as component property name,
+   * actual value as component property name list or as component property name with defined value to test expectations against.
+   * @param contain "The value to look for."
+   * @param [expectationFailOutput] Fail output.
+   */
+  contain<V = any>(actualOrContain: Argument<V>, contain?: V, expectationFailOutput?: any): this {
+    this.matcher<V>('contain', actualOrContain, contain, expectationFailOutput);
 
     return this;
   }
 
-  equal<V>(propertiesOrExpected: Data<V>, expected?: V): this {
-    if (this.beforeResult) {
-      this._equal<V>(propertiesOrExpected, this.beforeResult);
+  /**
+   * Expect the actual value to be defined. (Not undefined)
+   * @template V Expected type.
+   * @param [actualOrExpected] Acts as expected value when chaining, actual value as component property name,
+   * actual value as component property name list or as component property name with defined value to test expectations against.
+   * @param [expectationFailOutput] Fail output.
+   */
+  defined<V>(actualOrExpected?: Argument<V>, expectationFailOutput?: any): this {
+    this.matcher('defined', actualOrExpected, undefined, expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Actual value to be equal to the expected value.
+   * @template V Expected type.
+   * @param actualOrExpected Acts as expected value when chaining, actual value as component property name,
+   * actual value as component property name list or as component property name with defined value to test expectations against.
+   * @param [expected] "The actual value to be equal to the expected, using deep equality comparison".
+   * @param [expectationFailOutput] Fail output.
+   * @tested #12-21
+   */
+  equal<V>(actualOrExpected: Argument<V>, expected?: V, expectationFailOutput?: any): this {
+    this.matcher('equal', actualOrExpected, expected, expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Expect the actual value to be falsy.
+   * @template V Expected type.
+   * @param [actualOrPropertyName] Actual computed values or as component properties keys values to test expectations against false. 
+   * @param [expectationFailOutput] Fail output.
+   */
+  falsy<V>(actualOrPropertyName?: Argument<V>, expectationFailOutput?: any): this {
+    this.matcher('falsy', actualOrPropertyName, undefined, expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Expect the actual value to be null.
+   * @template V Expected type.
+   * @param [actualOrPropertyName] Actual computed values or as component properties keys values to test expectations against false. 
+   * @param [expectationFailOutput] Fail output.
+   */
+  null<V>(actualOrPropertyName?: Argument<V>, expectationFailOutput?: any): this {
+    this.matcher('null', actualOrPropertyName, undefined, expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Expect the actual value to be truthy.
+   * @param [actualOrPropertyName] Actual computed values or as component properties keys values to test expectations against truthy.
+   * @param [expectationFailOutput] Fail output.
+   */
+  truthy<V = boolean>(actualOrPropertyName?: Argument<V>, expectationFailOutput?: any): this {
+    this.matcher('truthy', actualOrPropertyName, undefined, expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Name of component properties or actual computed values to check expectation against undefined.
+   * @param [actualOrPropertyName] Actual computed values or as component properties keys values to test expectations against undefined. 
+   * @param [expectationFailOutput] Fail output.
+   */
+  undefined<V>(actualOrPropertyName?: Argument<V>, expectationFailOutput?: any): this {
+    this.matcher('undefined', actualOrPropertyName, undefined, expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Actual value to be expected value.
+   * @template V Expected type.
+   * @param actualOrPropertyName Actual computed value or value from component property key to test expectations against.
+   * @param expected "The actual value to be equal to the expected, using deep equality comparison".
+   * @param [expectationFailOutput] Fail output.
+   */
+  private _be<V>(actualOrPropertyName: Argument<V>, expected: V, expectationFailOutput?: any): this {
+    this
+      ._expect(actualOrPropertyName)
+      .toBe(expected, expectationFailOutput);
+    
+    return this;
+  }
+
+  /**
+   * Expect the actual value to contain a specific value.
+   * @template V Expected type.
+   * @param actualOrPropertyName Actual computed value or value from component property key to test expectations against.
+   * @param expected "The value to look for."
+   * @param [expectationFailOutput] Fail output.
+   */
+  private _contain<V>(actualOrPropertyName: Argument<V>, expected: V, expectationFailOutput?: any): this {
+    this
+      ._expect(actualOrPropertyName)
+      .toContain(expected, expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Check expectation against defined.
+   * @template V Expected type.
+   * @param actualOrPropertyName Actual computed value or value of component property to test expectations against.
+   * @param [expectationFailOutput] Fail output.
+   */
+  private _defined<V>(actualOrPropertyName: Argument<V>, expectationFailOutput?: any): this {
+    this
+      ._expect(actualOrPropertyName)
+      .toBeDefined(expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   * Actual value to be equal to expected value.
+   * @template V Expected type.
+   * @param actualOrPropertyName Actual computed value or value from component property key to test expectations against.
+   * @param expected "The actual value to be equal to the expected, using deep equality comparison".
+   * @param [expectationFailOutput] Fail output.
+   */
+  private _equal<V>(actualOrPropertyName: Argument<V>, expected: V, expectationFailOutput?: any): this {
+    this
+      ._expect(actualOrPropertyName)
+      .toEqual(expected, expectationFailOutput);
+    
+    return this;    
+  }
+
+  /**
+   * Expect just returned result or from parameter.
+   * @template V `propertyName` type.
+   * @param actualOrPropertyName Actual computed value or value from component property key to test expectations against.
+   */
+  private _expect<V>(actualOrPropertyName: Argument<V>): jasmine.Matchers<any> {
+    // First expected looks stored value in beforeResult, next 
+    const e = expect(this.actual(actualOrPropertyName));
+
+    // Store expect result.
+    const result = (this._not === true) ? e.not : e;
+
+    // Resetting _not after use.
+    this._not = false;
+    
+    return result;
+  }
+
+  /**
+   * Check expectation against to false.
+   * @template V Expected type.
+   * @param actualOrPropertyName Actual computed value or value from component property key to test expectations against.
+   * @param [expectationFailOutput] Fail output.
+   */
+  private _falsy<V>(actualOrPropertyName: Argument<V>, expectationFailOutput?: any): this {
+    this
+      ._expect(actualOrPropertyName)
+      .toBeFalsy(expectationFailOutput);
+
+    return this;    
+  }
+
+  private _null<V>(actualOrPropertyName: Argument<V>, expectationFailOutput?: any): this { 
+    this
+      ._expect(actualOrPropertyName)
+      .toBeNull(expectationFailOutput);
+    
+    return this;
+  }
+
+  /**
+   * Check expectation against truthy.
+   * @template V Expected type.
+   * @param actualOrPropertyName Actual computed value or value from component property key to test expectations against.
+   * @param [expectationFailOutput] Fail output.
+   */
+  private _truthy<V>(actualOrPropertyName: Argument<V>, expectationFailOutput?: any): this {
+    this
+      ._expect(actualOrPropertyName)
+      .toBeTruthy(expectationFailOutput);
+
+    return this;    
+  }
+
+  /**
+   * Check expectation against undefined.
+   * @template V Expected type.
+   * @param actualOrPropertyName Actual computed value or value of component property to test expectations against.
+   * @param [expectationFailOutput] Fail output.
+   */
+  private _undefined<V>(actualOrPropertyName: Argument<V>, expectationFailOutput?: any): this {
+    this
+      ._expect(actualOrPropertyName)
+      .toBeUndefined(expectationFailOutput);
+
+    return this;
+  }
+
+  /**
+   *
+   *
+   * @template V Expected type.
+   * @param name Matcher method name to call.
+   * @param actualOrExpected Acts as expected value when chaining, actual value as component property name, 
+   * actual value as component property name list or as component property name with defined value to test expectations against.
+   * @param [expected] Expected value to test expectations against.
+   * @param [expectationFailOutput] Fail output.
+   */
+  private matcher<V>(name: Matcher, actualOrExpected: Argument<V>, expected?: V, expectationFailOutput?: any): this {
+    // If result is defined.
+    if (this.result[this.result.name] !== undefined && typeGuard<V>(actualOrExpected)) {
+      this.switch<V>(name, undefined, actualOrExpected, expectationFailOutput);
+      // If result is not defined.
     } else {
-      this.toArray(propertiesOrExpected)
-        .forEach(property => {
-          if (expected) {
-            this._equal<V>(property, expected);
-          } else {
-            this._equal<V>(property, propertiesOrExpected[property]);
+      this
+        .toArray<V>(actualOrExpected)
+        .forEach((actual: V) => {
+          const actualOrIndex: V = actual;
+
+          // If expected is not undefined.
+          if (expected !== undefined) {
+            // If `Observable` detected.
+            if (typeof actual === 'string' && actual.endsWith('$')) {
+              let i = 0;
+              this.subscribe(actual, value => {
+                // console.log(`${value} ${name} ${expected[i]}`);
+                this.switch(name, value, expected[i]);
+                i++;
+              });
+            } else {
+              this.switch<V>(name, actualOrIndex, expected, expectationFailOutput);
+            }
+          } else if (expected === undefined) {
+            if (
+              typeof actualOrIndex === 'string' &&
+              typeGuard<{ [property: string]: V }>(actualOrExpected) &&
+              actualOrExpected[actualOrIndex] !== undefined
+            ) {
+              // And if actualOrExpected is json object.
+              this.switch<V>(name, actualOrIndex, actualOrExpected[actualOrIndex], expectationFailOutput);
+            } else if (typeGuard<V>(actualOrIndex)) {
+              this.switch<V>(name, actualOrIndex, expected, expectationFailOutput);
+            }
           }
         });
     }
@@ -59,123 +318,21 @@ export class MatchersClass<T> extends MainClass<T> {
     return this;
   }
 
-  falsy<V>(propertiesOrExpected?: Data<V>): this {
-    if (this.beforeResult !== undefined) {
-      if (typeof propertiesOrExpected === 'boolean') {
-        this._falsy(propertiesOrExpected);
-      }
-    } else {
-      this.toArray(propertiesOrExpected)
-        .forEach(property => this._falsy(property));
+  private switch<V>(name: Matcher, actual: Argument<V>, expected?: V, expectationFailOutput?: any): this {
+    switch (name) {
+      case 'be': this._be<V>(actual, expected, expectationFailOutput); break;
+      case 'contain': this._contain<V>(actual, expected, expectationFailOutput); break;
+      case 'equal': this._equal<V>(actual, expected, expectationFailOutput); break;
+
+      // expected is always undefined.
+      case 'defined': this._defined<V>(actual, expectationFailOutput); break;
+      case 'falsy': this._falsy<V>(actual, expectationFailOutput); break;
+      case 'null': this._null<V>(actual, expectationFailOutput); break;
+      case 'truthy': this._truthy<V>(actual, expectationFailOutput); break;
+      case 'undefined': this._undefined<V>(actual, expectationFailOutput); break;
+      default: break;
     }
 
     return this;
-  }
-
-  /**
-   * @param properties x
-   */
-  truthy<V>(properties: Data<V>): this {
-    this.toArray(properties)
-      .forEach(property => this._truthy<V>(property));
-
-    return this;
-  }
-
-  /**
-   * Name of component properties or actual computed values to check expectation against undefined.
-   * @param [propertiesOrValues] Actual computed values or as component properties keys values to test expectations against undefined. 
-   */
-  undefined<V>(propertiesOrValues?: Data<V>): this {
-    this.toArray(propertiesOrValues)
-      .forEach(property => this._undefined<V>(property));
-
-    return this;
-  }
-
-  /*
-    protected
-  */
-
-  /**
-   * 
-   * @template V Expected type.
-   * @param propertyOrValue Actual computed value or as component property key value to test expectations against.
-   * @param expected "The actual value to be equal to the expected, using deep equality comparison".
-   * @param [expectationFailOutput] -
-   */
-  protected _be<V>(propertyOrValue: string, expected: V, expectationFailOutput?: any): this {
-    this._expect(propertyOrValue)
-      .toBe(expected, expectationFailOutput);
-    
-    return this;
-  }
-
-  /**
-   * @template V Expected type.
-   * @param propertyOrValue Computed value or as component property key value to test expectations against.
-   * @param expected "The actual value to be equal to the expected, using deep equality comparison".
-   * @param [expectationFailOutput] -
-   */
-  protected _equal<V>(propertyOrValue: Data<V>, expected: V, expectationFailOutput?: any): this {
-    this._expect(propertyOrValue)   
-      .toEqual(expected, expectationFailOutput);
-    
-    return this;    
-  }
-
-  /**
-   * Expect just returned result to `beforeResult` or from parameter `propertyOrValue`.
-   * @template V Type of "actual computed value to test expectations against".
-   * @param propertyOrValue Computed value or as component property key value to test expectations against.
-   */
-  protected _expect<V>(propertyOrValue: Data<V>): jasmine.Matchers<any> {
-    return expect((this.beforeResult)
-      ? this.beforeResult : (typeof propertyOrValue === 'string')
-      ? get(this.comp, propertyOrValue) : propertyOrValue);
-  }
-
-  /**
-   * Check expectation against to false.
-   * @param propertyOrValue Actual computed value or as component property key value to test expectations against.
-   */
-  protected _falsy<V>(propertyOrValue: Data<V>): this {
-    this._expect(propertyOrValue)
-      .toBeFalsy();
-
-    return this;    
-  }
-
-  /**
-   * Check expectation against to truthy.
-   * @param propertyOrValue Actual computed value or as component property key value to test expectations against.
-   */
-  protected _truthy<V>(propertyOrValue: Data<V>): this {
-    this._expect(propertyOrValue)
-      .toBeTruthy();
-
-    return this;    
-  }
-
-  /**
-   * Check expectation against to undefined.
-   * @param propertyOrValue x
-   */
-  protected _undefined<V>(propertyOrValue: Data<V>): this {
-    this._expect(propertyOrValue)
-      .toBeUndefined();
-
-    return this;
-  }
-
-  /**
-   * @param selector Find HTMLElement by selector.
-   */
-  protected test(selector: string): void {
-    this.lastDebugElement = undefined;
-    this.lastDebugElement = this.debugElement.query(By.css(selector));
-    expect(this.lastDebugElement)
-      .not
-      .toBeNull();
   }
 }
