10809e5ee2d085b4cef7fc2cb5f9a23b14013adc
angularpackage <angular-package@wwwdev.io>
2018-07-17 17:53:51 +0200

    refactor(PrefixSuffixClass): Change because of tslint.
    Removed:
    - member access.
    - some jsdoc.

--- packages/core/packages/src/prefixsuffix.class.ts
+++ packages/core/packages/src/prefixsuffix.class.ts
@@ -1,22 +1,18 @@
 /**
  * @export
- * @class PrefixSuffixClass
  */
 export class PrefixSuffixClass {
-   /**
+  /**
    * Creates an instance of PrefixSuffixClass.
-   * @param {string} [prefix='_'] Source property prefix - property that will be connected with targeted dynamic component.
-   * @param {string} [suffix=''] Source property suffix - property that will be connected with targeted dynamic component.
-   * @memberof PrefixSuffixClass
+   * @param [prefix='_'] Source property prefix - property that will be connected with targeted dynamic component.
+   * @param [suffix=''] Source property suffix - property that will be connected with targeted dynamic component.
    */
   constructor(private prefix: string = '_', private suffix: string = '') { }
 
   /**
-   * @param {string} property
-   * @returns {string}
-   * @memberof PrefixSuffixClass
+   * @param property x
    */
-  public propertyName(property: string): string {
+  propertyName(property: string): string {
     return `${this.prefix}${property}${this.suffix}`;
   }
 }
