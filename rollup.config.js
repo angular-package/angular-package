cc086ceed13c78b95a92696a0620ec3715145fb7
angularpackage <angular-package@wwwdev.io>
2018-07-17 17:54:14 +0200

    chore(rollup): Import uglify different way.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import angular from 'rollup-plugin-angular';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
