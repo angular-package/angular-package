6bf03179eb30b96c1c5df4f873cf415211aa703e
angularpackage <angular-package@wwwdev.io>
2018-07-23 17:02:21 +0200

    refactor(index): Remove `SpecIt` from exporting.

--- packages/core/packages/testing/type/index.ts
+++ packages/core/packages/testing/type/index.ts
@@ -1,4 +1,4 @@
 export { ConsoleLog } from './console-log.type';
 export { Execute } from './execute.type';
 export { ResultName } from './result-name.type';
-export { SpecIt } from './spec-it.type';
+export { Testing } from './testing.type';
