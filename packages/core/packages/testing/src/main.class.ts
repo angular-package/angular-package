9619874a51323d5d5adee7859bdecead83c2c657
angularpackage <angular-package@wwwdev.io>
2018-07-23 20:59:49 +0200

    refactor(MainClass): Add/remove/update methods.
    Added:
    - `getResult()` method to get last result.
    
    Removed:
    - `actual()` method - it is no longer in use.
    - `describe()` method - move to another file.
    
    Updated:
    - `before()` and `set()` method to check `componentInstance` is defined.
    - `clear()` method to set specific result to undefined.

--- packages/core/packages/testing/src/main.class.ts
+++ packages/core/packages/testing/src/main.class.ts
@@ -1,25 +1,22 @@
 // external
-import { async, TestBed, TestModuleMetadata } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 // internal
-import { Argument } from '../../type';
 import { typeGuard } from '../../src';
-import { Settings } from '../interface';
 import { ResultName } from '../type';
 import { PropertiesClass } from './properties.class';
 
 export abstract class MainClass<T> extends PropertiesClass<T> {
-
   /**
-   * Do some operations on component before expectation.
+   * Make some operations on component before expectation.
    * @param callback Function with injected component and `this` object.
    */
   before(callback: (component: T, testingClass?: MainClass<T>) => any): this {
     this.clear('before');
-    this.result.before = callback(this.componentInstance, this);
-    this.result.name = 'before';
+    if (this.componentInstance) {
+      this.result.before = callback(this.componentInstance, this);
+      this.result.name = 'before';  
+    }
 
     return this;
   }
@@ -31,83 +28,26 @@ export abstract class MainClass<T> extends PropertiesClass<T> {
   clear(name?: ResultName): this {
     if (name) {
       this.result[name] = undefined;
+      this.result.name = undefined;
     } else {
       this.result.query = undefined;
       this.result.before = undefined;
-    }
+    }  
 
     return this;
   }
-
-  /**
-   * Primary describe with environment and module definition.
-   * @param specToExecute Tests to execute.
-   * @param [description=this.description] Jasmine textual description of the main group.
-   * @param [moduleDef=this.moduleDef] Angular module definition.
-   */
-  describe(
-    specToExecute: Function,
-    description: string = this.description,
-    moduleDef: TestModuleMetadata = this.moduleDef
-  ): this {
-
-    // Environment.
-    beforeAll(() => {
-      TestBed.resetTestEnvironment();
-      TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-    });
-    
-    // Main describe.
-    describe(description, () => {
-
-      // Angular TestBed configure and compile.
-      beforeEach(async(() => {
-        TestBed
-          .configureTestingModule(moduleDef)
-          .compileComponents();
-
-        this.fixture = TestBed.createComponent(this.componentTest);
-      }));
-
-      // Create component fixture.
-      // beforeEach(async(() => { });
-
-      /* Default spec to check fixture and comp is defined.
-      if (this.componentTest !== undefined) {
-        it('should have fixture and comp defined.', async(() => {
-          expect(this.fixture)
-            .toBeDefined();
-          expect(this.componentInstance)
-            .toBeTruthy();
-        }));  
-      }
-      */
-
-      // Execute tests.
-      if (specToExecute) {
-        specToExecute();
-      }
-    });
-    
-    return this;
-  }
-
-  /*
-  has(path: any): boolean {
-    // console.info(this.componentInstance['__component'].instance.model);
-    console.info(has(this.componentInstance, '__component.instance.model'));
-
-    return this.propertyClass.has(this.componentInstance, path);
-  }
-  */
 
   /**
    * Get component property value by using lodash `get()` function.
    * @template PT Returned component property value type.
    * @param [path] Component property name (key).
    */
-  get<PT>(path: string): PT {
-    return this.propertyClass.get<PT>(this.componentInstance, path);
+  get<PT>(path: string): PT | undefined {
+    if (this.componentInstance !== undefined) {
+      return this.propertyClass.get<PT>(this.componentInstance, path);
+    }
+
+    return;
   }
 
   /**
@@ -117,13 +57,15 @@ export abstract class MainClass<T> extends PropertiesClass<T> {
    * @param [value] Component property value.
    */
   set<PT>(path: string, value: PT): this {
-    this.propertyClass.set<PT>(this.componentInstance, path, value);
+    if (this.componentInstance !== undefined) {
+      this.propertyClass.set<PT>(this.componentInstance, path, value);
+    }
 
     return this;
   }
 
   subscribe<PT>(propertyName: string, callback: Function): this {
-    const observable: Observable<PT> = this.get<Observable<PT>>(propertyName);
+    const observable: Observable<PT> | undefined = this.get<Observable<PT>>(propertyName);
     let i = 0;
     if (typeGuard<Observable<PT>>(observable)) {
       observable.subscribe(
@@ -142,54 +84,29 @@ export abstract class MainClass<T> extends PropertiesClass<T> {
   }
 
   /**
-   * Actual value from method `before()` returned result, last debguElement query or from component property.
-   * @template PT Returned value type.
-   * @param actualOrPropertyName Component property name.
-   */
-  protected actual<PT>(actualOrPropertyName?: Argument<PT>): PT {
-    // Get actual from `before()` method or debugElement query result.
-    if (this.result[this.result.name] !== undefined) {
-      if (this.result.name === 'query') {
-        return this.result[this.result.name].nativeElement.innerHTML;
-      }
-
-      return this.result[this.result.name];
-    }
-    // Get actual value from component property by using `propertyName`.
-    if (this.propertyClass.string(actualOrPropertyName)) {
-      if (this.get<PT>(actualOrPropertyName)) {
-        const propertyValue = this.get<PT>(actualOrPropertyName);
-        if (typeGuard<PT>(propertyValue)) {
-          return propertyValue;
-        }
-      }
-
-      return;
-    }
-
-    /*
-    if (typeGuard<PT>(actualOrPropertyName)) {
-      return actualOrPropertyName;
-    }
-    */
-  }
-
-  /**
    * @param options ts
    * @param [number] Number of spec to execute.
    */
-  protected execution(settings: Settings, number?: number): boolean {
-    if (typeof settings.execute === 'boolean') {
-      return settings.execute;
+  protected execution(number: number): boolean {
+    if (typeof this.settings.execute === 'boolean') {
+      return this.settings.execute;
     }
 
     return (
-      settings.execute instanceof Array &&
+      this.settings.execute instanceof Array &&
       (
-        settings.execute.length === 0
+        this.settings.execute.length === 0
         ||
-        (settings.execute.length > 0 && number > 0 && settings.execute.includes(number))
+        (number !== undefined && number > 0 && this.settings.execute.length > 0 && this.settings.execute.includes(number))
       )
     );
+  }
+
+  protected getResult<TYPE>(): TYPE {
+    if (this.result.name === 'query' && this.result.query !== undefined) {
+      return this.result.query.nativeElement.innerHTML;
+    }
+
+    return this.result.before;
   }
 }
