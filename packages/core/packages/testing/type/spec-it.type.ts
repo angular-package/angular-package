2148d0a86e8a8ed0fd4ddf1a312802c532f89ba0
angularpackage <angular-package@wwwdev.io>
2018-07-23 17:01:55 +0200

    refactor(SpecIt): Remove - it is no longer in used due to create new instances.

--- packages/core/packages/testing/type/spec-it.type.ts
+++
@@ -1,6 +0,0 @@
-import { Spec, SpecSettings } from '../interface';
-
-/**
- * Type of callback function executed when using `eachIt()` method.
- */
-export type SpecIt = (specsName: Array<string>, specs: Spec, settings: SpecSettings, execution: (number: number) => boolean) => any;
