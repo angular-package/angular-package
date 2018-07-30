fcbbd9bccc83f7d0aabb4d4145011dd6948bff95
angularpackage <angular-package@wwwdev.io>
2018-07-30 14:05:58 +0200

    refactor(TestingClassMethods): Simplify interface name.

--- packages/core/packages/testing/interface/testing.interface.ts
+++ packages/core/packages/testing/interface/testing.interface.ts
@@ -2,7 +2,7 @@
 import { ConsoleLog, Execute } from '../type';
 import { Spec } from './spec.interface';
 
-export interface TestingClassMethods<T> {
+export interface Testing<T> {
   execute(execute?: Execute, log?: ConsoleLog): this;
   spec(description: string, spec: Spec<T>, reset: boolean): this;
 }
