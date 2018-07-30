f16f5bdae42a8ec6567441fd76b10750f9eca281
angularpackage <angular-package@wwwdev.io>
2018-07-30 20:58:33 +0200

    refactor(Property): Add return `this` to methods to have possibility to chain.

--- packages/core/packages/property/interface/property.interface.ts
+++ packages/core/packages/property/interface/property.interface.ts
@@ -2,11 +2,10 @@ import { Getter, Setter } from '../type';
 
 export interface Property {
   binded: string | Array<string> | number;
-  bind<S, T = string>(source: Function | S, properties: string | Array<string>, target: T): void;
-  clear<S>(source: Function | S, properties?: string | Array<string>): void;
-  define<S>(source: Function | S, property: string, descriptor: ThisType<any>): void;
+  bind<S, T = string>(source: Function | S, properties: string | Array<string>, target: T): this;
+  clear<S>(source: Function | S, properties?: string | Array<string>): this;
   get<PT>(source: Object, path: string): PT;
   set<PT>(source: Object, path: string, value: PT): Object;
   string(object: any): object is string;
-  wrap<S, R = any>(source: Function | S, properties: string | Array<string>, setter: Setter<S, R>, getter: Getter<S, R>): void;
+  wrap<S, R = any>(source: Function | S, properties: string | Array<string>, setter: Setter<S, R>, getter: Getter<S, R>): this;
 }
