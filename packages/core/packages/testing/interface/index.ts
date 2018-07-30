39cda9bee1e243a3db274492d741fc1ce1a438e7
angularpackage <angular-package@wwwdev.io>
2018-07-30 14:04:43 +0200

    refactor(interface): Update export names.

--- packages/core/packages/testing/interface/index.ts
+++ packages/core/packages/testing/interface/index.ts
@@ -1,8 +1,8 @@
 export { Log } from './log.interface';
-export { MatchersClassMethods } from './matchers-class-methods.interface';
+export { Matchers } from './matchers.interface';
 export { Options } from './options.interface';
 export { Result } from './result.interface';
 export { Spec } from './spec.interface';
 export { SpecSettings } from './spec-settings.interface';
 export { Settings } from './settings.interface';
-export { TestingClassMethods } from './testing-class-methods.interface';
+export { Testing } from './testing.interface';
