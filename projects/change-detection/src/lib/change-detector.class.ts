17a241c1d3dcffb9283be0734ecdbdb1067f6b7a
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-23 00:24:15 +0100

    style: reorder imports

--- projects/change-detection/src/lib/change-detector.class.ts
+++ projects/change-detection/src/lib/change-detector.class.ts
@@ -1,5 +1,7 @@
-import { DetectionProperties } from '../interface/detection-properties.interface';
 import { ChangeDetectorRef } from '@angular/core';
+
+// internals
+import { DetectionProperties } from '../interface/detection-properties.interface';
 
 /**
  * Helper class for component.
