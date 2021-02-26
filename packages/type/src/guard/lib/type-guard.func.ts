649d9cbe468eeac929723d54c4d2329ab157d625
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-26 02:29:56 +0100

    refactor: change name from typeGuard to isPrimitiveType and use Primitives type

--- packages/type/src/guard/lib/type-guard.func.ts
+++
@@ -1,4 +0,0 @@
-import { Types } from '../../lib/types.type';
-
-export const typeGuard = <Type>(value, type: Types<Type>): value is Type =>
-  (typeof type === 'string') ? typeof value === type : value instanceof type;
