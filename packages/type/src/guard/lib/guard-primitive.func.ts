1c66e2a9eff44770bd4b3832c3867b392d09b324
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-28 17:49:15 +0100

    refactor(isPrimitive): now isPrimitiveType is known as guardPrimitive with the same functionality

---
+++ packages/type/src/guard/lib/guard-primitive.func.ts
@@ -0,0 +1,7 @@
+import { Primitives } from '../../lib/primitives.type';
+/**
+ * Guard the `value` to be `Type` from `Primitives`.
+ * @param value to guard.
+ * @param type to check `value` value is `Primitive` type.
+ */
+export const guardPrimitive = <Type>(value: Type, type: Primitives): value is Type => typeof value === type;
--- packages/type/src/guard/lib/is-primitive-type.func.ts
+++
@@ -1,7 +0,0 @@
-import { Primitives } from '../../lib/primitives.type';
-/**
- * Guard the `value` to be `Type` and check its value is `Primitives` type.
- * @param value to guard.
- * @param type to check `value` value is `Primitive` type.
- */
-export const isPrimitiveType = <Type>(value: Type, type: Primitives): value is Type => typeof value === type;
