a45cdd7c1010af84a1fae50b8a5bf37f9c108d36
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-28 18:11:11 +0100

    docs(guardType): add information about isType

--- packages/type/src/guard/lib/guard-type.func.ts
+++ packages/type/src/guard/lib/guard-type.func.ts
@@ -1,7 +1,9 @@
 import { Types } from '../../lib/types.type';
 /**
  * Guard the `value` to be `Type` and check its value is `Primitives` type.
+ * Use `isType()` function for check ONLY.
  * @param value to guard.
  * @param type to check `value` value is `Primitive` type.
  */
-export const guardType = <Type>(value: Type, type: Types<Type>): value is Type => typeof value === type;
+export const guardType = <Type>(value: Type, type: Types<Type>): value is Type =>
+  (typeof type === 'string') ? (typeof value === type) : value instanceof type;
