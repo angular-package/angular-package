1b303ef201d29b45ada43eacf3774d0a894c5ff9
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-28 18:20:08 +0100

    refactor: change directory for checkers

---
+++ packages/type/src/check/index.ts
@@ -0,0 +1,6 @@
+export { isFunction } from './lib/is-function.func';
+export { isNumber } from './lib/is-number.func';
+export { isObject } from './lib/is-object.func';
+export { isPrimitive } from './lib/is-primitive.func';
+export { isString } from './lib/is-string.func';
+export { isType } from './lib/is-type.func';
