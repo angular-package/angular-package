7f9861bd4fe62837312fb55df7c2695cac0ccff4
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-28 20:51:29 +0100

    docs(isFunction): update

--- packages/type/src/check/lib/is-function.func.ts
+++ packages/type/src/check/lib/is-function.func.ts
@@ -1,8 +1,8 @@
 import { FunctionType } from '../../lib/function.type';
 /**
  * Check function `func` by finding `name` in `func`.
- * @param name `string` value to find in `func`.
- * @param func function type value to find `name` in.
+ * @param name Value `string` to find in argument `func`.
+ * @param func Function type value to find `name` in.
  * @returns boolean.
  */
 export const isFunction = (name: string, func: FunctionType): func is FunctionType => name in func;
