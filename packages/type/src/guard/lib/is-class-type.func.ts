1e106d6f74a4d505a85b09bc53235b02a6e806c7
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-28 18:11:34 +0100

    refactor(isClass): add todo and remove line

--- packages/type/src/guard/lib/is-class-type.func.ts
+++ packages/type/src/guard/lib/is-class-type.func.ts
@@ -1,9 +1,8 @@
-import { EBADF } from 'node:constants';
 import { Constructor } from '../../lib/constructor.type';
 /**
  * Check any `object` is generic `Type`.
  * @param object Object class to guard and check instance.
  * @param type y
  */
-
+// TODO:: check for proper functionality.
 export const isClass = <Type>(object: any, type?: Constructor<Type>): object is Type => object instanceof type;
