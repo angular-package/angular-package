2a8091013200de627f68b4160eb22a4833347a21
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-28 20:47:04 +0100

    docs(isPrimitive): update

--- packages/type/src/check/lib/is-primitive.func.ts
+++ packages/type/src/check/lib/is-primitive.func.ts
@@ -1,8 +1,8 @@
 import { Primitives } from '../../lib/primitives.type';
 /**
- * Check is the `value` a `Type` from `Primitives`.
- * Use `guardPrimitive()` function for full `Primitives` guard.
- * @param value to guard.
- * @param type to check `value` value is `Primitive` type.
+ * Check is any `value` a generic `Type` type from one of the `Primitives`.
+ * Use `guardPrimitive()` function to type guard generic `Type` `value` also.
+ * @param value Any value to check it is a generic `Type` from the `type`.
+ * @param type One of the `Primitives` `'boolean'`, `'bigint'`, `'number'`, `'string'` type to check `value`.
  */
 export const isPrimitive = <Type>(value: any, type: Primitives): value is Type => typeof value === type;
