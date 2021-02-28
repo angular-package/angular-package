0bee58307038877a4bd8040c2f6a996d07369091
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-02-28 18:11:01 +0100

    docs(guardString): add information about isString

--- packages/type/src/guard/lib/guard-string.func.ts
+++ packages/type/src/guard/lib/guard-string.func.ts
@@ -1,5 +1,6 @@
 /**
  * Guard the `value` to be a `string` type.
+ * Use `isString()` function for check ONLY.
  * @param value to guard.
  */
 export const guardString = (value: string): value is string => typeof value === 'string';
